497d3c3eb8e1a847d2c37f044f4538e249935b62
Frederique <frederique.degroen@deltares.nl>
2020-11-06 13:36:48 +0100

    updated config to have test=True or False as input

--- ra2ce/utils.py
+++ ra2ce/utils.py
@@ -1,25 +1,29 @@
 import os
 import json
 
-def load_config():
+
+def load_config(test=False):
     """
     Read config.json
     NOTE: make sure your working directory is set to the highest level folder in the directory
 
     """
-    config_path = os.path.join('test_config.json')
+    config_path = './config.json'
+    if test:
+        config_path = './test_config.json'
     with open(config_path, 'r') as config_fh:
         config = json.load(config_fh)
     return config
 
+
 if __name__=='__main__':
     print("Testing if the paths in the config file already exist")
-    config = load_config()
-    for key in config['paths']:
-        path = config['paths'][key]
-        print('{} : "{}"'.format(key,path))
-        print(os.path.exists(path),end="\n\n")
+    cfg = load_config()
+    for key in cfg['paths']:
+        path = cfg['paths'][key]
+        print('{} : "{}"'.format(key, path))
+        print(os.path.exists(path), end="\n\n")
 
     print("This is a demo of how to use the load_config: ")
-    demo_path = load_config()['paths']['test_network_shp']
+    demo_path = load_config()['paths']['network_shp']
     print(demo_path)
