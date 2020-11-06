539c03f16fb558e59dbae133b9be61579076a6b7
Kees van Ginkel <kees.vanginkel@deltares.nl>
2020-11-06 17:13:51 +0100

    Made init and setup.py

--- ra2ce/__init__.py
+++ ra2ce/__init__.py
@@ -3,3 +3,24 @@
 __author__ = """Margreet van Marle"""
 __email__ = 'Margreet.vanMarle@deltares.nl'
 __version__ = '0.1.0'
+
+print('Started configuring RA2CE. This is awesome.')
+
+import os, sys, logging
+from pathlib import Path
+folder = Path(__file__).parents[0]
+print(folder)
+#sys.path.append(folder)
+
+#LOG_FILENAME = os.path.join(os.path.dirname(folder), './log_ra2ce.log')
+LOG_FILENAME = folder / 'log_ra2ce.log'
+if LOG_FILENAME.exists():
+    print('er was er al eentje')
+else: print('er was er geen')
+
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S',
+                    filename=LOG_FILENAME,
+                    level=logging.INFO)
+
+
