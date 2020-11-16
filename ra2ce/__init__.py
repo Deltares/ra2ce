0714ffec6393595993449af0f899a1ab8a54b86e
Kees van Ginkel <kees.vanginkel@deltares.nl>
2020-11-16 11:44:32 +0100

    Updated initialisation for use as a module

--- ra2ce/__init__.py
+++ ra2ce/__init__.py
@@ -1,22 +1,23 @@
 """Top-level package for Risk Assessment and Adaptation for Critical infrastructurE."""
 
-__author__ = """Margreet van Marle"""
+__author__ = """Frederique de Groen, Kees van Ginkel, Margreet van Marle, Amine Aboufirass"""
 __email__ = 'Margreet.vanMarle@deltares.nl'
 __version__ = '0.1.0'
 
-print('Started configuring RA2CE. This is awesome.')
+print('Started configuration of RA2CE from .ini file in folder:')
 
 import os, sys, logging
 from pathlib import Path
+
 folder = Path(__file__).parents[0]
 print(folder)
-#sys.path.append(folder)
 
-#LOG_FILENAME = os.path.join(os.path.dirname(folder), './log_ra2ce.log')
+#In the same way, we can import the other packages
+import ra2ce.analyses_direct
+print('"ra2ce.analyses_direct" imported')
+
+
 LOG_FILENAME = folder / 'log_ra2ce.log'
-if LOG_FILENAME.exists():
-    print('er was er al eentje')
-else: print('er was er geen')
 
 logging.basicConfig(format='%(asctime)s - %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S',
