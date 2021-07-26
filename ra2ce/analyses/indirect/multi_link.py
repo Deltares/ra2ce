333ea3a45f608edead80403263c81dbc7cac498a
Frederique <frederique.degroen@deltares.nl>
2021-07-26 18:03:32 +0200

    first day RA2CE sprint July 2021

--- ra2ce/analyses/indirect/multi_link.py
+++ ra2ce/analyses/indirect/multi_link.py
@@ -4,3 +4,23 @@ Created on 26-7-2021
 
 @author: F.C. de Groen, Deltares
 """
+
+
+class MultiLink:
+    def __init__(self):
+        self.bla = 'bla'
+
+    def calculate_redundancy(self):
+        return
+
+    def origin_destination_known(self):
+        return
+
+    def origin_destination_unknown(self):
+        return
+
+    def regional_rerouting(self):
+        return
+
+    def execute(self):
+        return
