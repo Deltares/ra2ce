5c3540cb8ce138dbebb8bf0bf8c282c7a29bfcd2
Frederique <frederique.degroen@deltares.nl>
2021-08-05 14:59:36 +0200

    Moved load_config to utils.py

--- ra2ce/ra2ce.py
+++ ra2ce/ra2ce.py
@@ -8,37 +8,15 @@ Created on 26-7-2021
 from pathlib import Path
 
 # Local modules
-from utils import parse_config, initiate_root_logger, configure_analyses
-from checks import input_validation
+from utils import parse_config, initiate_root_logger, configure_analyses, load_config
 from graph.networks import Network
 from analyses.direct import analyses_direct
 from analyses.indirect import analyses_indirect
-from shutil import copyfile
+
 
 def main():
     # Find the settings.ini file
     root_path = Path(__file__).resolve().parent.parent
-
-
-    def load_config(root_path, type):
-        # Read the configurations in network.ini and add the root path to the configuration dictionary.
-        settings = root_path / "{}.ini".format(type)
-        config = parse_config(root_path, path=settings)
-        config['root_path'] = root_path
-
-        # Validate the configuration input.
-        config = input_validation(config)
-
-        if type == 'analyses':
-            # Create a dictionary with direct and indirect analyses separately.
-            config = configure_analyses(config)
-
-        # Set the output paths in the configuration Dict for ease of saving to those folders.
-        config['input'] = config['root_path'] / 'data' / config['project']['name'] / 'input'
-        config['static'] = config['root_path'] / 'data' / config['project']['name'] / 'static'
-        config['output'] = config['root_path'] / 'data' / config['project']['name'] / 'output'
-        copyfile(settings, config['output'] / '{}.ini'.format(type))
-        return config
 
     config_network = load_config(root_path, type='network')
     config_analyses = load_config(root_path, type='analyses')
