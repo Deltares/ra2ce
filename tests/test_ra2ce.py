5d5dae202db00997e35a3b6f71505e95d5d11255
Frederique <frederique.degroen@deltares.nl>
2021-08-06 11:01:13 +0200

    Testing works locally

--- tests/test_ra2ce.py
+++ tests/test_ra2ce.py
@@ -4,22 +4,21 @@
 
 from pathlib import Path
 import pytest
-
 from click.testing import CliRunner
-
-from ra2ce import ra2ce
-# from ra2ce import cli
-
 import pandas as pd
 
+from ra2ce import ra2ce
 
-def lookup_output(output_path=Path('data/test/output/correct'), case='single_link_redundancy'):
+
+def lookup_output(output_path, case):
     """Read an output file"""
-    test_path = output_path / case / f'{case}_test.csv'
+    root_path = Path(__file__).resolve().parent.parent
+    test_path = Path(output_path) / case / f'{case}_test.csv'
+    if not test_path.is_file():
+        test_path = root_path / test_path
     return pd.read_csv(test_path)
 
 
-# @pytest.mark.skip(reason="work in progress")
 def test_output():
     """Sample pytest test function for output"""
     cases = ['single_link_redundancy', 'multi_link_redundancy', 'optimal_route_origin_destination',
@@ -28,17 +27,22 @@ def test_output():
     input_analyses_path = Path('data/test/analyses.ini')
     ra2ce.main(input_network_path, input_analyses_path)
 
+    # to_check = ['ra2ce_fid', 'alt_dist', 'alt_nodes', 'diff_dist']
+
     for case in cases:
-        outputs_correct = lookup_output(case)
-        outputs_test = lookup_output(Path('data/test/output'), case)
+        outputs_correct = lookup_output('data/test/output/correct', case)
+        outputs_test = lookup_output('data/test/output', case)
 
         # Check if the pandas dataframes are equal in all aspects
+        print(case)
         pd.testing.assert_frame_equal(outputs_test, outputs_correct)
 
 
 @pytest.mark.skip(reason="work in progress")
 def test_command_line_interface():
     """Test the CLI."""
+    from ra2ce import cli
+
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
