75b2685b09152738c8729cb1dbb8bc09ea0997ae
Frederique <frederique.degroen@deltares.nl>
2021-11-02 11:40:02 +0100

    added run.py to enable automated testing in github

---
+++ run.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 6-8-2021
+
+@author: F.C. de Groen, Deltares
+"""
+
+from ra2ce.ra2ce import main
+
+if __name__ == '__main__':
+    main(r"D:\ra2ceMaster\ra2ce\data\test\network.ini", r"D:\ra2ceMaster\ra2ce\data\test\analyses.ini")
