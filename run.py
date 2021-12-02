0d9eea41ba70c803d10d26e80a18da9712a66364
Frederique <frederique.degroen@deltares.nl>
2021-12-02 14:46:35 +0100

    Added QA report Carles

--- run.py
+++ run.py
@@ -9,5 +9,5 @@ from ra2ce.ra2ce import main
 
 if __name__ == '__main__':
 
-    rootpath = r'c:\Python\ra2ce\data\2_test_network_pbf'
+    rootpath = r'D:\ra2ceMaster\ra2ce\data\test'
     main(rootpath + r"\network.ini", rootpath + r"\analyses.ini")
