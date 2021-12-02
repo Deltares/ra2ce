76aab8151ae52e4e985c9cacbc54fa5a21671b71
MartijnKwant <88429155+MartijnKwant@users.noreply.github.com>
2021-12-02 13:17:21 +0100

    Create run_with_args.bat

---
+++ run_with_args.bat
@@ -0,0 +1,2 @@
+call C:\Users\bkm\Anaconda3\Scripts\activate.bat C:\Users\bkm\Anaconda3\envs\igraph
+C:\Users\bkm\Anaconda3\envs\igraph\python.exe D:\ProbabilisticTools\models\Ra2ce\run_with_args.py D:\ProbabilisticTools\models\Ra2ce\KBN2_losses\data12\network.ini D:\ProbabilisticTools\models\Ra2ce\KBN2_losses\data12\analyses.ini
