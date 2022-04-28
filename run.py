ee62a0c364869a6e7a23b097b81c69e252c5b225
Frederique <frederique.degroen@deltares.nl>
2022-04-28 17:24:44 +0200

    Added command line interface to run.py.

--- run.py
+++ run.py
@@ -5,12 +5,16 @@ Created on 6-8-2021
 @author: F.C. de Groen, Deltares
 """
 
+from pathlib import Path
+import click
 from ra2ce.ra2ce import main
 
-if __name__ == '__main__':
-    rootpath = r'c:\Python\ra2ce\data\KBN2_losses'
-    main(rootpath + r"\network.ini", rootpath + r"\analyses.ini")
-    # rootpath = r'c:\Python\ra2ce\data\8_test_shape_with_od_disruption'
-    # main(rootpath + r"\network.ini", rootpath + r"\analyses.ini")
-    # rootpath = r'c:\Python\ra2ce\data\1_test_network_shape'
-    # main(rootpath + r"\network.ini", rootpath + r"\analyses.ini")
+
+@click.command()
+@click.option("--project", default=Path(__file__).resolve().parent / 'data' / 'test', help="Full path to the project folder of the RA2CE database.")
+def cli(project):
+    main(project + r"\network.ini", project + r"\analyses.ini")
+
+
+if __name__ == "__main__":
+    cli()
