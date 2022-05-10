3fb0666802427cc535f1cbcf137907df92799bc3
Frederique <frederique.degroen@deltares.nl>
2022-05-10 12:00:49 +0200

    Removed the duplicated CLI

--- run.py
+++ run.py
@@ -5,17 +5,18 @@ Created on 6-8-2021
 @author: F.C. de Groen, Deltares
 """
 
-from pathlib import Path
 import click
 from ra2ce.ra2ce import main
 
 
 @click.command()
-@click.option("--project", default=Path(__file__).resolve().parent / 'data' / 'test', help="Full path to the project folder of the RA2CE database.")
-def cli(project):
-    main(project + r"\network.ini", project + r"\analyses.ini")
+@click.option("--network_ini", default=None, help="Full path to the network.ini file.")
+@click.option("--analyses_ini", default=None, help="Full path to the analyses.ini file.")
+def cli(network_ini, analyses_ini):
+    main(network_ini, analyses_ini)
 
 
 if __name__ == "__main__":
     cli()
-    # main(r"D:\ra2ceMaster\ra2ce\data\Nepal_shp_test\network.ini", r"D:\ra2ceMaster\ra2ce\data\Nepal_shp_test\analyses.ini")
+    # r"D:\ra2ceMaster\ra2ce\data\Nepal_shp_test\analyses.ini"
+    # main(r"D:\ra2ceMaster\ra2ce\data\Nepal_shp_test\network.ini")
