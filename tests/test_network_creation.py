49da5e19015e673f2651abfc70d2daca1796e492
Frederique <frederique.degroen@deltares.nl>
2022-06-08 15:48:27 +0200

    Added 2 tests and test data

--- tests/test_network_creation.py
+++ tests/test_network_creation.py
@@ -20,3 +20,12 @@ def test_1_network_shape():
     main(network_ini=network_ini)
     check_output_files(test_name, ['1_network_shape_lines_that_merged.shp', 'base_graph.gpickle', 'base_network.feather'])
 
+
+def test_3_network_osm_download():
+    """To test the graph and network creation from a shapefile. Also applies line segmentation for the network."""
+    from ra2ce.ra2ce import main
+
+    test_name = "3_network_osm_download"
+    network_ini, ana_conf = get_paths(test_name)
+    main(network_ini=network_ini)
+    check_output_files(test_name, ['base_graph.gpickle', 'base_network.feather', 'simple_to_complex.json', 'complex_to_simple.json'])
