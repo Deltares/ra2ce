eeb43bcb5eae2f3972458adade7c13acc5147779
Frederique <frederique.degroen@deltares.nl>
2022-06-08 16:53:06 +0200

    Final update tests for today

--- tests/test_network_creation.py
+++ tests/test_network_creation.py
@@ -5,9 +5,6 @@ Created on 8-6-2022
 @author: F.C. de Groen, Deltares
 """
 
-from pathlib import Path
-import pandas as pd
-import pytest
 from tests.test_ra2ce import get_paths, check_output_files
 
 
@@ -16,7 +13,7 @@ def test_1_network_shape():
     from ra2ce.ra2ce import main
 
     test_name = "1_network_shape"
-    network_ini, ana_conf = get_paths(test_name)
+    network_ini, analyses_ini = get_paths(test_name)
     main(network_ini=network_ini)
     check_output_files(test_name, ['1_network_shape_lines_that_merged.shp', 'base_graph.gpickle', 'base_network.feather'])
 
@@ -26,6 +23,7 @@ def test_3_network_osm_download():
     from ra2ce.ra2ce import main
 
     test_name = "3_network_osm_download"
-    network_ini, ana_conf = get_paths(test_name)
+    network_ini, analyses_ini = get_paths(test_name)
     main(network_ini=network_ini)
     check_output_files(test_name, ['base_graph.gpickle', 'base_network.feather', 'simple_to_complex.json', 'complex_to_simple.json'])
+
