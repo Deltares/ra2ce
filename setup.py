fcc2e2d69c80f06befbe4f100878d0aefd33d631
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-12 14:15:09 +0200

    fix(setup.py): Encoding for the rst files should be enforced

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open('README.rst', encoding="utf8") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.rst', encoding="utf8") as history_file:
     history = history_file.read()
 
 requirements = ['Click>=7.0', ]
