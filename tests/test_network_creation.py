56a27e02fe635a71f13db22eeb6b7014d3ec80b7
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-13 14:26:20 +0200

    ci: Corrected python version in incubator step

--- tests/test_network_creation.py
+++ tests/test_network_creation.py
@@ -5,25 +5,36 @@ Created on 8-6-2022
 @author: F.C. de Groen, Deltares
 """
 
+from ra2ce.ra2ce import main
 from tests.test_ra2ce import check_output_graph_files, get_paths
 
 
 def test_1_network_shape():
     """To test the graph and network creation from a shapefile. Also applies line segmentation for the network."""
-    from ra2ce.ra2ce import main
-
     test_name = "1_network_shape"
     network_ini, analyses_ini = get_paths(test_name)
     main(network_ini=network_ini)
-    check_output_graph_files(test_name, ['1_network_shape_lines_that_merged.shp', 'base_graph.p', 'base_network.feather'])
+    check_output_graph_files(
+        test_name,
+        [
+            "1_network_shape_lines_that_merged.shp",
+            "base_graph.p",
+            "base_network.feather",
+        ],
+    )
 
 
 def test_3_network_osm_download():
     """To test the graph and network creation from a shapefile. Also applies line segmentation for the network."""
-    from ra2ce.ra2ce import main
-
     test_name = "3_network_osm_download"
     network_ini, analyses_ini = get_paths(test_name)
     main(network_ini=network_ini)
-    check_output_graph_files(test_name, ['base_graph.p', 'base_network.feather', 'simple_to_complex.json', 'complex_to_simple.json'])
-
+    check_output_graph_files(
+        test_name,
+        [
+            "base_graph.p",
+            "base_network.feather",
+            "simple_to_complex.json",
+            "complex_to_simple.json",
+        ],
+    )
