b044abf9926bb208e5c6f9766821b4b3818a2635
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-16 19:12:30 +0200

    feat(ra2ce_handler.py): Applied Factory patter for analysis runner

--- ra2ce/ra2ce_handler.py
+++ ra2ce/ra2ce_handler.py
@@ -11,44 +11,77 @@ warnings.filterwarnings(action="ignore", message="All-NaN slice encountered")
 warnings.filterwarnings(action="ignore", message="Value *not successfully written.*")
 
 
-from typing import Optional
+from typing import Any, Optional, Protocol
 
 from ra2ce.analyses.direct import analyses_direct
 from ra2ce.analyses.indirect import analyses_indirect
+from ra2ce.configuration.analysis_ini_configuration import AnalysisIniConfigurationBase
 from ra2ce.ra2ce_input import Ra2ceInput
 
 # Local modules
 from ra2ce.utils import initiate_root_logger
 
 
-def analysis_handler(network_config: dict, analysis_config: dict, graphs: dict) -> None:
-    """Directs the script to the right functions to run the analyses
+class AnalysisRunner(Protocol):
+    @staticmethod
+    def can_run(ra2ce_input: Ra2ceInput) -> bool:
+        pass
 
-    Args:
-        network_config: Network configuration dict, from the network.ini input file
-        analysis_config: Analysis configuration dict, from the analyses.ini input file
-        graphs: Dictionary of a network (GDF) and graph(s) (NetworkX Graph)
+    def run(self, analysis_config: AnalysisIniConfigurationBase) -> None:
+        pass
 
-    Returns:
-        Nothing
-    """
-    # Do the analyses
-    try:
-        if "direct" in analysis_config:
-            if network_config["hazard"]["hazard_map"] is not None:
-                analyses_direct.DirectAnalyses(analysis_config, graphs).execute()
-            else:
-                logging.error(
-                    "Please define a hazardmap in your network.ini file. Unable to calculate direct damages..."
-                )
 
-        if "indirect" in analysis_config:
-            analyses_indirect.IndirectAnalyses(analysis_config, graphs).execute()
+class DirectAnalysisRunner(AnalysisRunner):
+    def __str__(self) -> str:
+        return "Direct Analysis Runner"
 
-    except BaseException as e:
-        logging.exception(
-            f"RA2CE crashed. Check the logfile for the Traceback message: {e}"
-        )
+    @staticmethod
+    def can_run(ra2ce_input: Ra2ceInput) -> bool:
+        _network_config = ra2ce_input.network_config.config_data
+        if not ("direct" in ra2ce_input.analysis_config.config_data):
+            return False
+        if not _network_config["hazard"]["hazard_map"]:
+            logging.error(
+                "Please define a hazardmap in your network.ini file. Unable to calculate direct damages."
+            )
+            return False
+        return True
+
+    def run(self, analysis_config: AnalysisIniConfigurationBase) -> None:
+        analyses_direct.DirectAnalyses(
+            analysis_config.config_data, analysis_config.graphs
+        ).execute()
+
+
+class IndirectAnalysisRunner(AnalysisRunner):
+    def __str__(self) -> str:
+        return "Indirect Analysis Runner"
+
+    @staticmethod
+    def can_run(ra2ce_input: Ra2ceInput) -> bool:
+        return super().can_run(ra2ce_input)
+
+    def run(self, analysis_config: AnalysisIniConfigurationBase) -> None:
+        analyses_indirect.IndirectAnalyses(
+            analysis_config.config_data, analysis_config.graphs
+        ).execute()
+
+
+class AnalysisRunnerFactory:
+    @staticmethod
+    def get_runner(ra2ce_input: Ra2ceInput) -> AnalysisRunner:
+        _available_runners = [DirectAnalysisRunner, IndirectAnalysisRunner]
+        _supported_runners = [
+            _runner for _runner in _available_runners if _runner.can_run(ra2ce_input)
+        ]
+        if not _supported_runners:
+            logging.error("No analysis runner found for the given configuration.")
+
+        if len(_supported_runners) > 1:
+            logging.warn(
+                f"More than one runner available, using {_supported_runners[0]}"
+            )
+        return _supported_runners[0]
 
 
 class Ra2ceHandler:
@@ -79,8 +112,10 @@ class Ra2ceHandler:
         if not self.input_config.is_valid_input():
             logging.error("Error validating input files. Ra2ce will close now.")
             sys.exit()
-        _network_config = self.input_config.network_config.config_data
-        _analysis_config = self.input_config.analysis_config.config_data
-        analysis_handler(
-            _network_config, _analysis_config, self.input_config.network_config.graphs
-        )
+        _runner: AnalysisRunner = AnalysisRunnerFactory.get_runner(self.input_config)
+        try:
+            _runner().run(self.input_config.analysis_config)
+        except BaseException as e:
+            logging.exception(
+                f"RA2CE crashed. Check the logfile for the Traceback message: {e}"
+            )
