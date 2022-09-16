38bd8769db4d40f30618aca0b25b047874fede63
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-16 09:09:11 +0200

    test(test_refactoring.py): Created separated test to avoid messing up with the original test suite

---
+++ tests/test_refactoring.py
@@ -0,0 +1,16 @@
+from ra2ce.ra2ce import Ra2ceInput
+from tests import test_data
+
+
+class TestRefactorings:
+    def test_given_input_gets_configurations(self):
+        root_test_dir = test_data / "acceptance_test_data"
+        _network_ini = root_test_dir / "network.ini"
+        _analysis_ini = root_test_dir / "analyses.ini"
+        assert _network_ini.is_file()
+        assert _analysis_ini.is_file()
+
+        _race_input = Ra2ceInput(_network_ini, _analysis_ini)
+
+        assert _race_input.validate_input()
+        assert _race_input.network
