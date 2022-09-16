ccd0f0145e07ea93182aee5b213e16b3ba8c31ae
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-16 19:22:03 +0200

    refactor(ra2ce/runners): Extracted runners logic into separate module

---
+++ ra2ce/runners/direct_analysis_runner.py
@@ -0,0 +1,28 @@
+import logging
+
+from ra2ce.analyses.direct import analyses_direct
+from ra2ce.configuration.analysis_ini_configuration import AnalysisIniConfigurationBase
+from ra2ce.ra2ce_input import Ra2ceInput
+from ra2ce.runners.analysis_runner_protocol import AnalysisRunner
+
+
+class DirectAnalysisRunner(AnalysisRunner):
+    def __str__(self) -> str:
+        return "Direct Analysis Runner"
+
+    @staticmethod
+    def can_run(ra2ce_input: Ra2ceInput) -> bool:
+        _network_config = ra2ce_input.network_config.config_data
+        if not ("direct" in ra2ce_input.analysis_config.config_data):
+            return False
+        if not _network_config["hazard"]["hazard_map"]:
+            logging.error(
+                "Please define a hazardmap in your network.ini file. Unable to calculate direct damages."
+            )
+            return False
+        return True
+
+    def run(self, analysis_config: AnalysisIniConfigurationBase) -> None:
+        analyses_direct.DirectAnalyses(
+            analysis_config.config_data, analysis_config.graphs
+        ).execute()
