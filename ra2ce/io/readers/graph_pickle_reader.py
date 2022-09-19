c17cc832d21c5bd73b4097a5cc1617ee8d60aaf7
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-19 13:48:08 +0200

    chore: Cleanup

--- ra2ce/io/readers/graph_pickle_reader.py
+++ ra2ce/io/readers/graph_pickle_reader.py
@@ -5,8 +5,10 @@ from typing import Any
 
 
 class GraphPickleReader:
-    def read(pickle_path: Path) -> Any:
+    def read(self, pickle_path: Path) -> Any:
         _read_graph = None
+        if not pickle_path:
+            raise ValueError(f"No pickle path was provided")
         if not pickle_path.is_file():
             logging.warning(f"No pickle found at path {pickle_path}")
             return None
