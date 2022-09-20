8c626b9dcbea56f654d23058c4a9e401cd591f3d
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-20 14:20:18 +0200

    chore: Renamed ra2ce_input to ra2ce_input_config. Updated imports / references

--- ra2ce/runners/analysis_runner_protocol.py
+++ ra2ce/runners/analysis_runner_protocol.py
@@ -1,12 +1,12 @@
 from typing import Protocol
 
 from ra2ce.configuration.analysis_ini_configuration import AnalysisIniConfigurationBase
-from ra2ce.ra2ce_input import Ra2ceInput
+from ra2ce.ra2ce_input_config import Ra2ceInputConfig
 
 
 class AnalysisRunner(Protocol):
     @staticmethod
-    def can_run(ra2ce_input: Ra2ceInput) -> bool:
+    def can_run(ra2ce_input: Ra2ceInputConfig) -> bool:
         pass
 
     def run(self, analysis_config: AnalysisIniConfigurationBase) -> None:
