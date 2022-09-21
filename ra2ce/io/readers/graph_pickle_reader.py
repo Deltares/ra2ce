c7d02fe7fd99987dc7596c93f48c6d4fcf133c7c
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-21 14:31:58 +0200

    test(tests/io/TestGraphPickleReader): Created tests for GraphPickleReader. Added extra test data dir

--- ra2ce/io/readers/graph_pickle_reader.py
+++ ra2ce/io/readers/graph_pickle_reader.py
@@ -10,10 +10,12 @@ class GraphPickleReader(FileReaderProtocol):
     def read(self, pickle_path: Path) -> Any:
         _read_graph = None
         if not pickle_path:
-            raise ValueError(f"No pickle path was provided")
+            raise ValueError(f"No pickle path was provided.")
+
         if not pickle_path.is_file():
-            logging.error(f"No pickle found at path {pickle_path}")
-            raise ValueError()
+            _error_mssg = f"No pickle found at path {pickle_path}"
+            logging.error(_error_mssg)
+            raise ValueError(_error_mssg)
 
         with open(pickle_path, "rb") as f:
             _read_graph = pickle.load(f)
