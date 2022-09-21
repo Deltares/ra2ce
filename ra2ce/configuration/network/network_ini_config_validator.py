1ae8a8ca946282661ca930bd31fa2b1e8979cf24
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-21 16:49:57 +0200

    test(config): Added validators for network and config_validator_base'

--- ra2ce/configuration/network/network_ini_config_validator.py
+++ ra2ce/configuration/network/network_ini_config_validator.py
@@ -5,14 +5,16 @@ from ra2ce.validation.validation_report import ValidationReport
 
 
 class NetworkIniConfigurationValidator(IniConfigValidatorBase):
-    def _validate_shp_input(self, network_config: dict) -> None:
+    def _validate_shp_input(self, network_config: dict) -> ValidationReport:
         """Checks if a file id is configured when using the option to create network from shapefile"""
-        if (network_config["source"] == "shapefile") and (
-            network_config["file_id"] is None
-        ):
-            self._report.error(
+        _shp_report = ValidationReport()
+        _source = network_config.get("source", None)
+        _file_id = network_config.get("file_id", None)
+        if (_source == "shapefile") and (not _file_id):
+            _shp_report.error(
                 "Not possible to create network - Shapefile used as source, but no file_id configured in the network.ini file"
             )
+        return _shp_report
 
     def validate(self) -> ValidationReport:
         """Check if input properties are correct and exist."""
@@ -30,6 +32,6 @@ class NetworkIniConfigurationValidator(IniConfigValidatorBase):
             "hazard",
             "cleanup",
         ]
-        self._validate_shp_input(_network_config)
+        _report.merge(self._validate_shp_input(_network_config))
         _report.merge(self._validate_headers(_required_headers))
         return _report
