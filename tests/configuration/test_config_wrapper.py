ba643729e937c72eb8a585f1288c5b982a5566d0
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-22 12:32:13 +0200

    test(test_config_factory.py): Added (failing) tests for ConfigFactory class

--- tests/configuration/test_config_wrapper.py
+++ tests/configuration/test_config_wrapper.py
@@ -15,55 +15,6 @@ class TestConfigWrapper:
         assert not _input_config.analysis_config
         assert not _input_config.network_config
 
-    def test_from_input_paths(self):
-        # 1. Define test data.
-        _test_dir = test_data / "simple_inputs"
-        assert _test_dir.is_dir()
-        _analysis_ini = _test_dir / "analysis.ini"
-        _network_ini = _test_dir / "network.ini"
-
-        assert _analysis_ini.is_file() and _network_ini.is_file()
-
-        # 2. Run test.
-        _input_config = ConfigWrapper.from_input_paths(_analysis_ini, _network_ini)
-
-        # 3. Verify final expectations.
-        assert _input_config
-        assert isinstance(_input_config.analysis_config, AnalysisConfigBase)
-        assert isinstance(_input_config.network_config, NetworkConfig)
-
-    def test_from_input_paths_given_only_analysis(self):
-        # 1. Define test data.
-        _test_dir = test_data / "simple_inputs"
-        assert _test_dir.is_dir()
-        _analysis_ini = _test_dir / "analysis.ini"
-
-        assert _analysis_ini.is_file()
-
-        # 2. Run test.
-        _input_config = ConfigWrapper.from_input_paths(_analysis_ini, None)
-
-        # 3. Verify final expectations.
-        assert _input_config
-        assert isinstance(_input_config.analysis_config, AnalysisConfigBase)
-        assert not _input_config.network_config
-
-    def test_from_input_paths_given_only_network(self):
-        # 1. Define test data.
-        _test_dir = test_data / "simple_inputs"
-        assert _test_dir.is_dir()
-        _network_ini = _test_dir / "network.ini"
-
-        assert _network_ini.is_file()
-
-        # 2. Run test.
-        _input_config = ConfigWrapper.from_input_paths(None, _network_ini)
-
-        # 3. Verify final expectations.
-        assert _input_config
-        assert isinstance(_input_config.network_config, NetworkConfig)
-        assert not _input_config.analysis_config
-
     @pytest.mark.parametrize(
         "network_ini",
         [
