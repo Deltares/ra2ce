76b09a6363194031f9f67aa7cadf226aa14e558d
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-22 14:06:44 +0200

    chore(AnalysisIniConfigData): Moved AnalysisIniConfigData logic to concrete classes

--- ra2ce/configuration/analysis/analysis_ini_config_data.py
+++ ra2ce/configuration/analysis/analysis_ini_config_data.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from ra2ce.configuration.analysis.analysis_ini_config_validator import (
     AnalysisIniConfigValidator,
     AnalysisWithoutNetworkConfigValidator,
@@ -8,20 +10,31 @@ from ra2ce.configuration.ini_config_protocol import IniConfigDataProtocol
 class AnalysisIniConfigData(IniConfigDataProtocol):
     @classmethod
     def from_dict(cls, dict_values) -> IniConfigDataProtocol:
-        _new_analysis_ini_config_data = cls()
-        _new_analysis_ini_config_data.update(**dict_values)
-        return _new_analysis_ini_config_data
+        raise NotImplementedError("Implement in concrete classes")
 
     def is_valid(self) -> bool:
         raise NotImplementedError("Implement in concrete classes")
 
+
 class AnalysisWithNetworkIniConfigData(AnalysisIniConfigData):
+    @classmethod
+    def from_dict(cls, dict_values) -> AnalysisWithNetworkIniConfigData:
+        _new_analysis_ini_config_data = cls()
+        _new_analysis_ini_config_data.update(**dict_values)
+        return _new_analysis_ini_config_data
+
     def is_valid(self) -> bool:
         _validation_report = AnalysisIniConfigValidator(self).validate()
         return _validation_report.is_valid()
 
 
 class AnalysisWithoutNetworkIniConfigData(AnalysisIniConfigData):
+    @classmethod
+    def from_dict(cls, dict_values) -> AnalysisWithoutNetworkIniConfigData:
+        _new_analysis_ini_config_data = cls()
+        _new_analysis_ini_config_data.update(**dict_values)
+        return _new_analysis_ini_config_data
+
     def is_valid(self) -> bool:
         _validation_report = AnalysisWithoutNetworkConfigValidator(self).validate()
         return _validation_report.is_valid()
