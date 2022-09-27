88d4dda3377e78c4a5ccb8dfc75b1b7aa86d5249
Kees van Ginkel <kees.vanginkel@deltares.nl>
2022-09-27 20:28:30 +0200

    Moved some of the logic

--- ra2ce/analyses/direct/direct_damage_calculation.py
+++ ra2ce/analyses/direct/direct_damage_calculation.py
@@ -376,9 +376,13 @@ class DamageNetworkReturnPeriods(DamageNetwork):
     def __init__(self, road_gdf, val_cols):
         # Construct using the parent class __init__
         DamageNetwork.__init__(self, road_gdf, val_cols)
+
         self.return_periods = set(
             [x.split("_")[1] for x in val_cols]
         )  # set of unique return_periods
+
+        if not len(self.return_periods) > 1:
+            raise ValueError('No return_period cols present in hazard data')
 
     ### Controlers for EAD calculation
     def main(self, damage_function):
@@ -386,6 +390,7 @@ class DamageNetworkReturnPeriods(DamageNetwork):
 
         Arguments:
             *damage_function* = damage function that is to be used, valid arguments are: 'HZ', 'OSD', 'MAN'
+            *manual_damage_functions* = ManualDamageFunctions object
 
         """
 
@@ -400,6 +405,9 @@ class DamageNetworkReturnPeriods(DamageNetwork):
 
         if damage_function == "OSD":
             self.calculate_damage_OSdaMage(events=self.return_periods)
+
+        if damage_function == "MAN":
+            self.calculate_damage_manual_functions(events=self.events, manual_damage_functions=manual_damage_functions)
 
 
 class DamageNetworkEvents(DamageNetwork):
@@ -419,12 +427,16 @@ class DamageNetworkEvents(DamageNetwork):
             [x.split("_")[1] for x in val_cols]
         )  # set of unique events
 
+        if not len(self.events) > 0:
+            raise ValueError('No event cols present in hazard data')
+
     ### Controler for Event-based damage calculation
     def main(self, damage_function, manual_damage_functions=None):
         """Controler for doing the EAD calculation
 
         Arguments:
             *damage_function* = damage function that is to be used, valid arguments are: 'HZ', 'OSD', 'MAN'
+            *manual_damage_functions* = ManualDamageFunctions object
 
         """
         assert len(self.events) > 0, "no return periods identified"
@@ -432,7 +444,6 @@ class DamageNetworkEvents(DamageNetwork):
         assert "fr" in self.stats, "inundated fraction (key: fr) is missing"
 
         self.do_cleanup_and_mask_creation()
-
 
         if damage_function == "HZ":
             self.calculate_damage_HZ(events=self.events)
@@ -443,4 +454,4 @@ class DamageNetworkEvents(DamageNetwork):
         if damage_function == "MAN":
             self.calculate_damage_manual_functions(events=self.events, manual_damage_functions=manual_damage_functions)
 
-        #self.replace_none_with_nan() #Todo, not sure if we need this
+        #
