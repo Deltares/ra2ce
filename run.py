055f9cb9f321bfc097bd5b9750c0c3e939a8c450
Frederique <frederique.degroen@deltares.nl>
2022-11-18 16:17:42 +0100

    added run.py, you can run RA2CE from this script.

---
+++ run.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from tests.test_acceptance import run_from_cli
+
+
+if __name__ == "__main__":
+    run_from_cli(network_ini=r"D:\ra2ceMaster\0_DATABASE\paramaribotest\network.ini",
+                 analysis_ini=r"D:\ra2ceMaster\0_DATABASE\paramaribotest\analyses.ini")
+
