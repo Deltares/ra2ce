ef24ea935d3562188fb1681958639187977766f3
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-02-14 15:08:10 +0100

    refactor(losses.py): Extracted class from analyses_indirect

--- tests/analyses/indirect/test_analyses_indirect.py
+++ tests/analyses/indirect/test_analyses_indirect.py
@@ -16,24 +16,4 @@ class TestIndirectAnalyses:
 
         # 3. Verify final expectations.
         assert isinstance(_indirect_analyses, IndirectAnalyses)
-
-class TestLosses:
-
-    def test_initialize(self):
-        # 1. Define test data
-        _config = {}
-        _analyses = {
-            "duration_event": None,
-            "duration_disruption": None,
-            "fraction_detour": None,
-            "fraction_drivethrough": None,
-            "rest_capacity": None,
-            "maximum_jam": None,
-            "partofday": None
-        }
-
-        # 2. Run test.
-        _losses = Losses(_config, _analyses)
-
-        # 3. Verify final expectations.
-        assert isinstance(_losses, Losses)
+        
