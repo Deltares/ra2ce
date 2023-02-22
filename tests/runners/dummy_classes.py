fb127c8a86f8a3b330a547ef90c12beef46a6a47
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-02-22 11:02:54 +0100

    test(test_analysis_runner_factory.py): Added coverage test

--- tests/runners/dummy_classes.py
+++ tests/runners/dummy_classes.py
@@ -6,7 +6,6 @@ class DummyAnalysisConfig(AnalysisConfigBase):
     def __init__(self) -> None:
         self.config_data = {}
 
-
 class DummyRa2ceInput(ConfigWrapper):
     def __init__(self) -> None:
         self.analysis_config = DummyAnalysisConfig()
--- tests/runners/test_analysis_runner_factory.py
+++ tests/runners/test_analysis_runner_factory.py
@@ -1,9 +1,8 @@
-from pathlib import Path
-from typing import Optional
-
 import pytest
 
 from ra2ce.runners.analysis_runner_factory import AnalysisRunnerFactory
+from ra2ce.runners.analysis_runner_protocol import AnalysisRunner
+from ra2ce.runners.indirect_analysis_runner import IndirectAnalysisRunner
 from tests.runners.dummy_classes import DummyRa2ceInput
 
 
@@ -16,3 +15,15 @@ class TestAnalysisRunnerFactory:
             str(exc_err.value)
             == "No analysis runner found for the given configuration."
         )
+
+    def test_get_runner_with_manny_supported_runners_returns_analysis_runner_instance(self):
+        # 1. Define test data.
+        _config_wrapper = DummyRa2ceInput()
+        _config_wrapper.analysis_config.config_data = {"indirect": 42, "direct": 24}
+        _config_wrapper.network_config.config_data = {"hazard": {"hazard_map": 4224}}
+
+        # 2. Run test.
+        _runner = AnalysisRunnerFactory.get_runner(_config_wrapper)
+
+        # 3. Verify final expectations.
+        assert isinstance(_runner, AnalysisRunner)
