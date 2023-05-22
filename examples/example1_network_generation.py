d4ed0e2303737cf966849d3a29b2087e7ec11012
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2023-05-22 16:51:56 +0200

    example project, network generation and visualisation

--- examples/example1_network_generation.py
+++ examples/example1_network_generation.py
@@ -1,8 +1,27 @@
 from pathlib import Path
 import geopandas as gpd
+import folium
+import webbrowser
 
 # Just to make sonar-cloud stop complaining.
 _network_ini_name = "network.ini"
+
+
+def visualise_map():
+    global root_dir
+    layers = []
+    file_path = root_dir / r'static\output_graph\base_graph.html'
+    for i, entity_type in enumerate(["edges", "nodes"]):
+        base_graph_entity_path = root_dir / rf'static\output_graph\base_graph_{entity_type}.gpkg'
+        gdf = gpd.read_file(base_graph_entity_path)
+        if i == 0:
+            base_graph_map = gdf.explore()
+            layers.append(base_graph_map)
+        else:
+            base_graph_map = gdf.explore(m=layers[i-1])
+        base_graph_map.save(file_path)
+    webbrowser.open(str(file_path))
+
 
 if __name__ == "__main__":
     from ra2ce.ra2ce_handler import Ra2ceHandler
@@ -12,12 +31,8 @@ if __name__ == "__main__":
     network_ini = root_dir / _network_ini_name
     assert network_ini.is_file()
 
-    # 2. When run test.
+    # When run test.
     handler = Ra2ceHandler(network=network_ini, analysis=None)  # you can also input only the network_ini
     handler.configure()  # this will configure (create) the network and do the overlay of the hazard map with the
     # network if there is any
-
-    base_graph_path = root_dir / r'static\output_graph\base_graph_edges.gpkg'
-    gdf = gpd.read_file(base_graph_path)
-    gpd.explore(gdf)
-
+    visualise_map()  # Show the created maps.
