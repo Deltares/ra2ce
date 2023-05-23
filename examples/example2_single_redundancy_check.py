68b641142d59184d6072ecfc002340771f096898
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2023-05-23 07:21:46 +0200

    generalised visualise_map() function, used in examples 1 and 2 (all examples so far)

--- examples/example2_single_redundancy_check.py
+++ examples/example2_single_redundancy_check.py
@@ -2,17 +2,36 @@ from pathlib import Path
 import geopandas as gpd
 import webbrowser
 
-# Just to make sonar-cloud stop complaining.
-# _network_ini_name = "network.ini"
 _analysis_ini_name = "analysis.ini"
+
+
+def visualise_map(file_name, file_path, map_entities=None):
+    global root_dir
+    file_full_path = Path(str(file_path / file_name) + '.html')
+    if map_entities is not None:
+        layers = []
+        for i, entity_type in enumerate(map_entities):
+            base_graph_entity_path = Path(str(file_path / file_name) + f'_{entity_type}.gpkg')
+            gdf = gpd.read_file(base_graph_entity_path)
+            if i == 0:
+                base_graph_map = gdf.explore()
+                layers.append(base_graph_map)
+            else:
+                base_graph_map = gdf.explore(m=layers[i-1])
+            base_graph_map.save(file_full_path)
+    else:
+        base_graph_path = Path(str(file_path / file_name) + '.gpkg')
+        gdf = gpd.read_file(base_graph_path)
+        base_graph_map = gdf.explore()
+        base_graph_map.save(file_full_path)
+    webbrowser.open(str(file_full_path))
+
 
 if __name__ == "__main__":
     from ra2ce.ra2ce_handler import Ra2ceHandler
+
     example_num = 2
     root_dir = Path(rf'C:\repos\ra2ce\examples\example_{example_num}')
-
-    # network_ini = root_dir / _network_ini_name
-    # assert network_ini.is_file()
 
     analysis_ini = root_dir / _analysis_ini_name
     assert analysis_ini.is_file()
@@ -22,3 +41,8 @@ if __name__ == "__main__":
     handler.configure()  # this will configure (create) the network and do the overlay of the hazard map with the
     # network if there is any
     handler.run_analysis()  # this will run the analysis
+
+    # Show the created maps.
+    map_file_path = root_dir / r'output\single_link_redundancy'
+    map_file_name = 'single_link_redundancy_test'
+    visualise_map(file_name=map_file_name, file_path=map_file_path, map_entities=["edges", "nodes"])
