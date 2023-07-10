1c8ea7edffb9a52f1634b5b7ca5b2ae55134610a
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-07-10 14:55:15 +0200

    test: Added marker and reference to external test dsata directory

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,5 +8,9 @@ _test_dir = Path(__file__).parent
 test_data = _test_dir / "test_data"
 acceptance_test_data = test_data / "acceptance_test_data"
 test_results = _test_dir / "test_results"
+test_external_data = _test_dir / "test_external_data"
 
 slow_test = pytest.mark.slow_test
+external_test = pytest.mark.skipif(
+    not test_external_data.exists(), reason="No external test data available."
+)
