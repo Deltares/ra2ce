bfae8e76a185e0cb1caad33f750f4eb5330371cf
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-07-11 16:23:52 +0200

    chore: Updated dockerfile

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,14 +1,20 @@
-from tests import test_examples
+import subprocess
+from tests import test_examples, test_results
 from pathlib import Path
 import pytest
 
 _jupyter_examples = [
-    pytest.param(_jupyter_notebook, id=_jupyter_notebook.replace("_", " ").capitalize())
+    pytest.param(
+        _jupyter_notebook, id=_jupyter_notebook.stem.replace("_", " ").capitalize()
+    )
     for _jupyter_notebook in test_examples.glob("*.ipynb")
 ]
 
 
 class TestExamples:
     @pytest.mark.parametrize("jupyter_example", _jupyter_examples)
-    def test_run_jupyter_from_examples_dir(self, jupyter_example: Path):
-        pytest.fail("Work in progress: {}".format(jupyter_example.name))
+    @pytest.mark.skip(reason="Work in progress")
+    def test_run_jupyter_from_examples_dir(
+        self, jupyter_example: Path, request: pytest.FixtureRequest
+    ):
+        pytest.fail("Work in progress.")
