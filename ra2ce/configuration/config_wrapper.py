5cae2224a75d72513ad85fe9a9004b9df01dc58f
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-07-17 09:38:42 +0200

    chore: Reverted config wrapper validation with no analysis configuration

--- ra2ce/configuration/config_wrapper.py
+++ ra2ce/configuration/config_wrapper.py
@@ -49,7 +49,7 @@ class ConfigWrapper:
         Returns:
             bool: Input parameters are valid for a ra2ce run.
         """
-        if self.analysis_config and not self.analysis_config.is_valid():
+        if not self.analysis_config or not self.analysis_config.is_valid():
             logging.error("No valid analyses.ini file provided. Program will close.")
             return False
 
@@ -57,7 +57,7 @@ class ConfigWrapper:
             logging.error("No valid network.ini file provided. Program will close.")
             return False
 
-        if self.network_config and self.analysis_config:
+        if self.network_config:
             if self.analysis_config.root_dir != self.network_config.root_dir:
                 logging.error(
                     "Root directory differs between network and analyses .ini files"
