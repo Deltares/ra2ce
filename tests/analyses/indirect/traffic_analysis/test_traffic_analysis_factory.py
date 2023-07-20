d2f919a344eed3ecaac0759d958eb2382f970127
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-07-20 09:06:17 +0200

    test: Extended current tests for traffic_analysis

--- tests/analyses/indirect/traffic_analysis/test_traffic_analysis_factory.py
+++ tests/analyses/indirect/traffic_analysis/test_traffic_analysis_factory.py
@@ -12,7 +12,9 @@ from ra2ce.analyses.indirect.traffic_analysis.traffic_analysis_factory import (
 )
 
 from tests import test_data, test_results
-from tests.analyses.indirect.traffic_analysis.test_equity_analysis import (
+from tests.analyses.indirect.traffic_analysis import (
+    valid_traffic_analysis_input,
+    TrafficAnalysisInput,
     import_from_csv,
 )
 
@@ -82,26 +84,36 @@ class TestTrafficAnalysisFactory:
         assert isinstance(_result, TrafficAnalysisBase)
         assert isinstance(_result, TrafficAnalysis)
 
-    def test_get_analysis_with_equity_data_returns_equity_analysis(self):
+    def test_get_analysis_with_equity_data_returns_equity_analysis(
+        self, valid_traffic_analysis_input: TrafficAnalysisInput
+    ):
         # 1. Define test data.
-        _equity_test_data = test_data.joinpath("equity_data")
-        _gdf_data = import_from_csv(_equity_test_data.joinpath("gdf_data.csv"))
-        _od_table_data = import_from_csv(
-            _equity_test_data.joinpath("od_table_data.csv")
+        valid_traffic_analysis_input.destination_names = ""
+        assert (
+            "values_prioritarian"
+            not in valid_traffic_analysis_input.od_table_data.columns
         )
-        _equity_data = pd.read_csv(_equity_test_data.joinpath("equity_data.csv"))
-        _destination_names = ""
-        assert "values_prioritarian" not in _od_table_data.columns
 
         # 2. Run test.
         _result = TrafficAnalysisFactory.get_analysis(
-            _gdf_data, _od_table_data, _destination_names, _equity_data
+            valid_traffic_analysis_input.gdf_data,
+            valid_traffic_analysis_input.od_table_data,
+            valid_traffic_analysis_input.destination_names,
+            valid_traffic_analysis_input.equity_data,
         )
 
         # 3. Verify expectations.
         assert isinstance(_result, TrafficAnalysisBase)
         assert isinstance(_result, EquityAnalysis)
-        pd.testing.assert_frame_equal(_result.gdf, _gdf_data)
-        pd.testing.assert_frame_equal(_result.od_table, _od_table_data)
-        pd.testing.assert_frame_equal(_result.equity_data, _equity_data)
-        assert "values_prioritarian" in _od_table_data.columns
+        pd.testing.assert_frame_equal(
+            _result.gdf, valid_traffic_analysis_input.gdf_data
+        )
+        pd.testing.assert_frame_equal(
+            _result.od_table, valid_traffic_analysis_input.od_table_data
+        )
+        pd.testing.assert_frame_equal(
+            _result.equity_data, valid_traffic_analysis_input.equity_data
+        )
+        assert (
+            "values_prioritarian" in valid_traffic_analysis_input.od_table_data.columns
+        )
