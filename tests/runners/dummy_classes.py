69176e334b755d908c268959b4ba0866d74844bb
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-07-26 13:38:44 +0200

    test: Updated dummy config

--- tests/runners/dummy_classes.py
+++ tests/runners/dummy_classes.py
@@ -9,6 +9,16 @@ class DummyAnalysisConfig(AnalysisConfigWrapperBase):
     def __init__(self) -> None:
         self.config_data = {}
 
+    @classmethod
+    def from_data(cls, **kwargs):
+        raise NotImplementedError()
+
+    def configure(self) -> None:
+        raise NotImplementedError()
+
+    def is_valid(self) -> bool:
+        raise NotImplementedError()
+
 
 class DummyRa2ceInput(ConfigWrapper):
     def __init__(self) -> None:
