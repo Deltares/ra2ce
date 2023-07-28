c058c06fc1ade8c7582cfd0ac319a075a35a922b
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-07-28 11:58:09 +0200

    test: Fixed failing test

--- tests/graph/network_wrappers/test_shp_network_wrapper.py
+++ tests/graph/network_wrappers/test_shp_network_wrapper.py
@@ -1,6 +1,5 @@
 from ra2ce.graph.network_config_data.network_config_data import (
-    CleanupSection,
-    NetworkSection,
+    NetworkConfigData,
 )
 from ra2ce.graph.network_wrappers.network_wrapper_protocol import NetworkWrapperProtocol
 from ra2ce.graph.network_wrappers.shp_network_wrapper import ShpNetworkWrapper
@@ -9,12 +8,12 @@ from ra2ce.graph.network_wrappers.shp_network_wrapper import ShpNetworkWrapper
 class TestShpNetworkWrapper:
     def test_init(self):
         # 1.Define test data.
-        _network_options = NetworkSection()
-        _cleanup_options = CleanupSection()
+        _config_data = NetworkConfigData()
 
-        _wrapper = ShpNetworkWrapper(_network_options, _cleanup_options, None, "")
+        # 2. Run test.
+        _wrapper = ShpNetworkWrapper(_config_data)
 
-        # Verify expectations.
+        # 3. Verify expectations.
         assert isinstance(_wrapper, ShpNetworkWrapper)
         assert isinstance(_wrapper, NetworkWrapperProtocol)
         assert _wrapper.crs.to_epsg() == 4326
