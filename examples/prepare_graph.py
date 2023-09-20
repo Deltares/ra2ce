a6fdf12876db603510c48036e26936b79b91e76c
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2023-09-20 17:38:43 +0200

    feat: multi layer network is made and the function is implemented

--- examples/prepare_graph.py
+++ examples/prepare_graph.py
@@ -1,6 +1,7 @@
 import pickle
 from pathlib import Path
 
+import networkx as nx
 from networkx import MultiDiGraph
 
 from examples.assignment_functions import _get_directed_graph, _create_layered_graph
@@ -8,12 +9,22 @@ from examples.assignment_functions import _get_directed_graph, _create_layered_g
 root_folder = Path(
     r'C:\Users\asgarpou\OneDrive - Stichting Deltares\Documents\Projects\Short involvements\Moonshot5_hackathon\data\Moonshot_5')
 
-with open(root_folder / "generated" / "graph.p", 'rb') as handle:
-    graph = pickle.load(handle)
+# with open(root_folder / "generated" / "graph.p", 'rb') as handle:
+#     graph = pickle.load(handle)
 
+graph = nx.MultiDiGraph()
+graph.add_node(1, demand=-31, od_id="destination")
+graph.add_node(4, demand=31, od_id="origin")
+graph.add_edge(1, 2, cost=3, capacity=40)
+graph.add_edge(1, 3, cost=6, capacity=10)
+graph.add_edge(2, 4, cost=1, capacity=20)
+graph.add_edge(3, 4, cost=2, capacity=10)
 
 directed_graph = _get_directed_graph(graph)
-layered_graph = _create_layered_graph(directed_graph)
+multi_layer_graph = _create_layered_graph(directed_graph)
+flow_dict = nx.min_cost_flow(multi_layer_graph)
+
+a = 1
 
 
 
