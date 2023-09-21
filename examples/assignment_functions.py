274fcd6e994ec3d3ee1ba6fd8d4fec405caae3ec
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2023-09-21 11:28:16 +0200

    feat: pos added for graph plotting

--- examples/assignment_functions.py
+++ examples/assignment_functions.py
@@ -1,11 +1,19 @@
+from typing import Union
+
 import networkx as nx
-from networkx import MultiGraph, MultiDiGraph
+from matplotlib import pyplot as plt
+from networkx import MultiGraph, MultiDiGraph, DiGraph
 
 
 def _get_directed_graph(graph: MultiGraph) -> MultiDiGraph:
     multi_digraph = nx.MultiDiGraph()
     for u, attr in graph.nodes(data=True):
-        multi_digraph.add_node(u, **attr)
+        if 'geometry' in attr.keys():
+            geom = attr['geometry']
+            pos = (geom.x, geom.y)
+        else:
+            pos = ()
+        multi_digraph.add_node(u, pos=pos, **attr)
     for u, v, key, attr in graph.edges(keys=True, data=True):
         multi_digraph.add_edge(u, v, **attr)
         multi_digraph.add_edge(v, u, **attr)
@@ -20,7 +28,7 @@ def _create_layered_graph(graph: MultiDiGraph) -> MultiDiGraph:
     for u, attr in graph.nodes(data=True):
         multi_layer_graph.add_node(u, **attr)
     for u, v, key, attr in graph.edges(keys=True, data=True):
-        attr['weight'] = int(round(attr['weight']*10e6, 0))
+        attr['weight'] = int(round(attr['weight'] * 10e6, 0))
         attr['capacity'] = int(attr['capacity'])
         multi_layer_graph.add_edge(u, v, **attr)
 
@@ -29,31 +37,37 @@ def _create_layered_graph(graph: MultiDiGraph) -> MultiDiGraph:
 
     # add virtual graph to the multi_layer graph
     for u, attr in virtual_graph.nodes(data=True):
-        attr = {k: v for k, v in attr.items() if k in attr.keys() and k not in ['demand', 'od_id']}
-        multi_layer_graph.add_node((str(u)+'_d'), **attr)
+        attr = {k: v for k, v in attr.items() if k in attr.keys() and k not in ['demand', 'od_id', 'pos']}
+        if 'geometry' in attr.keys():
+            geom = attr['geometry']
+            pos = (geom.x+10, geom.y+10)
+        else:
+            pos = ()
+        multi_layer_graph.add_node((str(u) + '_d'), pos=pos, **attr)
     for u, v, key, attr in virtual_graph.edges(keys=True, data=True):
         attr['weight'] = int(round(attr['weight'] * 10e6, 0))
         attr['capacity'] = int(max_capacity_graph * 100)
-        multi_layer_graph.add_edge((str(u)+'_d'), (str(v)+'_d'), **attr)
+        multi_layer_graph.add_edge((str(u) + '_d'), (str(v) + '_d'), **attr)
 
     # create links between graph and virtual graph in the multi_layer graph at each node
     for node, attr in graph.nodes(data=True):
-        multi_layer_graph.add_edge(node, str(node)+'_d',
+        multi_layer_graph.add_edge(node, str(node) + '_d',
                                    weight=max_weight_graph + 1, capacity=max_capacity_graph * 100)
-        multi_layer_graph.add_edge(str(node)+'_d', node,
+        multi_layer_graph.add_edge(str(node) + '_d', node,
                                    weight=0, capacity=max_capacity_graph * 100)
     return multi_layer_graph
 
-# def draw_graph():
-#     fig = plt.figure()
-#     ax = fig.add_subplot(111)
-#     edge_labels = dict([((n1, n2), attr['capacity'])
-#                         for n1, n2, attr in multi_layer_graph.edges(data=True)])
-#     pos = nx.spring_layout(multi_layer_graph)
-#     nx.draw(multi_layer_graph, pos, with_labels=True, arrows=True, node_size=5, node_color='r', font_color='m')
-#     nx.draw_networkx_edge_labels(
-#         multi_layer_graph, pos,
-#         edge_labels=edge_labels,
-#         font_color='red'
-#     )
-#     plt.show()
+
+def draw_graph(g: Union[DiGraph, MultiDiGraph]):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    edge_labels = dict([((n1, n2), attr['capacity'])
+                        for n1, n2, attr in g.edges(data=True)])
+    pos = nx.spring_layout(g)
+    nx.draw(g, pos, with_labels=True, arrows=True, node_size=5, node_color='r', font_color='m')
+    nx.draw_networkx_edge_labels(
+        g, pos,
+        edge_labels=edge_labels,
+        font_color='red'
+    )
+    plt.show()
--- examples/prepare_graph.py
+++ examples/prepare_graph.py
@@ -2,13 +2,14 @@ import pickle
 from pathlib import Path
 
 import networkx as nx
+import shapely.geometry
 from networkx import MultiDiGraph
 import numpy as np
 # importing networkx
 # importing matplotlib.pyplot
 import matplotlib.pyplot as plt
 # from ortools.graph.python import min_cost_flow
-from examples.assignment_functions import _get_directed_graph, _create_layered_graph
+from examples.assignment_functions import _get_directed_graph, _create_layered_graph, draw_graph
 
 root_folder = Path(
     r'C:\Users\asgarpou\OneDrive - Stichting Deltares\Documents\Projects\Short involvements\Moonshot5_hackathon\data\Moonshot_5')
@@ -17,39 +18,18 @@ root_folder = Path(
 #     graph = pickle.load(handle)
 
 graph = nx.MultiGraph()
-graph.add_node(1, demand=-2100, od_id="origin")
-graph.add_node(4, demand=2100, od_id="destination")
+graph.add_node(1, demand=-2100, od_id="origin", geometry=shapely.geometry.Point((1, 2)))
+graph.add_node(4, demand=2100, od_id="destination", geometry=shapely.geometry.Point((10, 20)))
 graph.add_edge(1, 2, weight=3, capacity=10)
 graph.add_edge(1, 3, weight=6, capacity=10)
 graph.add_edge(2, 4, weight=1, capacity=40)
 graph.add_edge(3, 4, weight=2, capacity=20)
 
 directed_graph = _get_directed_graph(graph)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-edge_labels = dict([((n1, n2), attr['capacity'])
-                    for n1, n2, attr in directed_graph.edges(data=True)])
-pos = nx.spring_layout(directed_graph)
-nx.draw(directed_graph, pos, with_labels=True, arrows=True, node_size=5, node_color='r', font_color='m')
-nx.draw_networkx_edge_labels(
-    directed_graph, pos,
-    edge_labels=edge_labels,
-    font_color='red'
-)
-plt.show()
+draw_graph(directed_graph)
 
 multi_layer_graph = _create_layered_graph(directed_graph)
-
-fig = plt.figure()
-ax = fig.add_subplot(111)
-edge_labels = dict([((n1, n2), attr['capacity'])
-                    for n1, n2, attr in multi_layer_graph.edges(data=True)])
-pos = nx.spring_layout(multi_layer_graph)
-nx.draw(multi_layer_graph, pos, with_labels=True, arrows=True, node_size=5, node_color='r', font_color='m')
-nx.draw_networkx_edge_labels(
-    multi_layer_graph, pos, label_pos=0.4, edge_labels=edge_labels, font_color='red')
-plt.show()
+draw_graph(multi_layer_graph)
 
 flow_dict = nx.min_cost_flow(multi_layer_graph)
 a = 1
