8acdaf00bd498d20271f1d06848c8a06c3053f7d
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-11-14 13:58:59 +0100

    chore: Updated test to run all available jupyter examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,9 +1,9 @@
-import subprocess
 from pathlib import Path
 
 import pytest
-
-from tests import test_examples, test_results
+from tests import test_examples
+from pytest_notebook.nb_regression import NBRegressionFixture, NBRegressionError
+from pytest_notebook.notebook import NBConfigValidationError
 
 _jupyter_examples = [
     pytest.param(
@@ -15,8 +15,20 @@ _jupyter_examples = [
 
 class TestExamples:
     @pytest.mark.parametrize("jupyter_example", _jupyter_examples)
-    @pytest.mark.skip(reason="Work in progress")
-    def test_run_jupyter_from_examples_dir(
-        self, jupyter_example: Path, request: pytest.FixtureRequest
-    ):
-        pass
+    def test_run_jupyter_from_examples_dir(self, jupyter_example: Path):
+        try:
+            _fixture = NBRegressionFixture(
+                exec_notebook=True,
+                exec_allow_errors=False,
+                diff_color_words=False,
+                force_regen=True,
+                exec_timeout=50
+            )
+            _fixture.check(str(jupyter_example), raise_errors=True)
+        except NBRegressionError:
+            # We only verify whether the execution was succesful.
+            # We DO NOT compare the execution to the previous generated results.
+            pass
+        except NBConfigValidationError:
+            # These are the sort errors we are looking for :)
+            raise
