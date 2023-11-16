cca54401ea612e886238012cd896a367e8799108
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-11-16 15:31:19 +0100

    ci: Updated make_exe to include version file

--- scripts/make_exe.py
+++ scripts/make_exe.py
@@ -3,13 +3,40 @@ import PyInstaller.__main__
 from ra2ce import main as cli_module
 from ra2ce import __version__
 from os import environ
+from pyinstaller_versionfile import create_versionfile
+from datetime import datetime
+
+_conda_env = Path(environ["CONDA_PREFIX"])
+_root_dir = Path(__file__).parent.parent
+_workpath = Path(__file__).parent.joinpath("build")
+
+_cli_name = "ra2ce_cli"
 
 
-def build_cli():
+def get_version_file() -> Path:
 
-    _conda_env = Path(environ["CONDA_PREFIX"])
-    _site_packages = _conda_env.joinpath("lib", "site-packages")
+    _version_file = _workpath.joinpath("version.rc")
+    create_versionfile(
+        _version_file,
+        version=__version__,
+        company_name="Deltares",
+        legal_copyright=f"Copyright (C) {datetime.now().year} Stichting Deltares",
+        product_name="Ra2ce",
+        original_filename=f"{_cli_name}.exe",
+    )
+    return _version_file
+
+
+def get_hidden_imports() -> list[str]:
+    """
+    Finds in the conda environment all the packages which could not be directly found
+    with `pyinstaller`.
+
+    Returns:
+        list[str]: List of `--hidden-import` arguments for the `pyinstaller` call.
+    """
     _hidden_imports = []
+    _site_packages = _conda_env.joinpath("lib", "site-packages")
 
     def add_hidden_imports(module_name: str, *extra_imports) -> list[str]:
         _hidden_imports.append(_site_packages.joinpath(module_name))
@@ -23,23 +50,28 @@ def build_cli():
     add_hidden_imports("rasterio", "_shim")
     add_hidden_imports("pygeos", "_geos")
     add_hidden_imports("pyogrio", "_geometry")
+    return list(f"--hidden-import={_hi}" for _hi in _hidden_imports)
 
-    _workpath = Path(__file__).parent.joinpath("build")
+
+def build_cli():
+
     _ra2ce_dir = Path(cli_module.__file__).parent
-    _root_dir = Path(__file__).parent.parent
+
     PyInstaller.__main__.run(
         [
             cli_module.__file__,
-            "--name=ra2ce_cli",
+            f"--name={_cli_name}",
             f"--paths=.",
             f"--paths={str(_conda_env)}",
             f"--paths={str(_ra2ce_dir)}",
             f"--paths={str(_root_dir)}",
-            *list(f"--hidden-import={_hi}" for _hi in _hidden_imports),
+            *get_hidden_imports(),
             f"--workpath={str(_workpath)}",
             f"--specpath={str(_workpath)}",
             # "--add-data={}:README.md".format(str(_root_dir.joinpath("README.md"))),
             # f"--icon={str(convert_icon)}",
+            "--copy-metadata=ra2ce",
+            f"--version-file={str(get_version_file())}",
             "--noconfirm",
             "--onefile",
             "--clean",
