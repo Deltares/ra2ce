a04891bb50d467090a2c90fdac641e91ec179c82
Ardt Klapwijk <ardt.klapwijk@deltares.nl>
2023-11-21 13:05:04 +0100

    chore: fix return type

--- ra2ce/graph/graph_files_collection.py
+++ ra2ce/graph/graph_files_collection.py
@@ -74,12 +74,12 @@ class NetworkFile(GraphFileProtocol):
     file: Path = None
     graph: GeoDataFrame = None
 
-    def read_graph_file(self, file: Path) -> MultiGraph:
+    def read_graph_file(self, file: Path) -> GeoDataFrame:
         self.file = file
         self.read_graph()
         return self.graph
 
-    def read_graph(self) -> MultiGraph:
+    def read_graph(self) -> GeoDataFrame:
         if self.file.is_file():
             self.graph = read_feather(self.file)
         return self.graph
