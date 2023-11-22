0da0548dccdebf48c5e1ffbf9d4cc5691981f465
Ardt Klapwijk <ardt.klapwijk@deltares.nl>
2023-11-22 15:47:36 +0100

    Merge branch 'master' into feature/248-create-objects-for-graph-and-files-in-config-wrappers

--- ra2ce/graph/hazard/hazard_files.py
+++ ra2ce/graph/hazard/hazard_files.py
@@ -5,5 +5,5 @@ from pathlib import Path
 @dataclass
 class HazardFiles:
     tif: list[Path] = field(default_factory=list)
-    shp: list[Path] = field(default_factory=list)
+    gpkg: list[Path] = field(default_factory=list)
     table: list[Path] = field(default_factory=list)
