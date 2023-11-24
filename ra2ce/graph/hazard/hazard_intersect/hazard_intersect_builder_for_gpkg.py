a0c6742e6bd8efeb1ef785efc11347d45eeedb2c
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2023-11-24 12:57:58 +0100

    chore: Removed unnecessary imports

--- ra2ce/graph/hazard/hazard_intersect/hazard_intersect_builder_for_gpkg.py
+++ ra2ce/graph/hazard/hazard_intersect/hazard_intersect_builder_for_gpkg.py
@@ -23,7 +23,6 @@ from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Callable
 from geopandas import GeoDataFrame, read_file, sjoin
-from joblib import Parallel, delayed
 from networkx import Graph
 from numpy import nanmean
 from ra2ce.graph.hazard.hazard_intersect.hazard_intersect_builder_base import (
