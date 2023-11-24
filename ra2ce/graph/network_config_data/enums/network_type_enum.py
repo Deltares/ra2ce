24513bd19cc9c7836b82f07abc7f88f60c6cdb3c
Ardt Klapwijk <ardt.klapwijk@deltares.nl>
2023-11-24 16:58:56 +0100

    chore: add network_type enum; process review comments

---
+++ ra2ce/graph/network_config_data/enums/network_type_enum.py
@@ -0,0 +1,10 @@
+from ra2ce.configuration.ra2ce_enum_base import Ra2ceEnumBase
+
+
+class NetworkTypeEnum(Ra2ceEnumBase):
+    WALK = 1
+    BIKE = 2
+    DRIVE = 3
+    DRIVE_SERVICE = 4
+    ALL = 5
+    INVALID = 99
