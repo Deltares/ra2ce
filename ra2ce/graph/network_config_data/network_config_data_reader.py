a73f5b71f2ba9def2b4fef76dc1c7f4f5da96bfa
Ardt Klapwijk <ardt.klapwijk@deltares.nl>
2023-11-27 12:46:06 +0100

    chore: add enum for road_types

--- ra2ce/graph/network_config_data/network_config_data_reader.py
+++ ra2ce/graph/network_config_data/network_config_data_reader.py
@@ -6,6 +6,7 @@ from ra2ce.common.configuration.ini_configuration_reader_protocol import (
     ConfigDataReaderProtocol,
 )
 from ra2ce.graph.network_config_data.enums.network_type_enum import NetworkTypeEnum
+from ra2ce.graph.network_config_data.enums.road_type_enum import RoadTypeEnum
 from ra2ce.graph.network_config_data.enums.source_enum import SourceEnum
 from ra2ce.graph.network_config_data.network_config_data import (
     CleanupSection,
@@ -161,8 +162,11 @@ class NetworkConfigDataReader(ConfigDataReaderProtocol):
         _network_section.network_type = NetworkTypeEnum.get_enum(
             self._parser.get(_section, "network_type", fallback=None)
         )
-        _network_section.road_types = self._parser.getlist(
-            _section, "road_types", fallback=_network_section.road_types
+        _network_section.road_types = list(
+            map(
+                RoadTypeEnum.get_enum,
+                self._parser.getlist(_section, "road_types", fallback=[None]),
+            )
         )
         _network_section.polygon = self._get_str_as_path(_network_section.polygon)
         return _network_section
