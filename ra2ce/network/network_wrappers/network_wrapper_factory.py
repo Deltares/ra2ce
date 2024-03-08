c484d39e02265b8b1255fc8994d4cbeefc051afa
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-03-08 12:06:39 +0100

    chore: add raise if invalid source

--- ra2ce/network/network_wrappers/network_wrapper_factory.py
+++ ra2ce/network/network_wrappers/network_wrapper_factory.py
@@ -72,3 +72,4 @@ class NetworkWrapperFactory(NetworkWrapperProtocol):
                 self.output_graph_dir.joinpath("base_network.feather")
             )
             return base_graph, network_gdf
+        raise ValueError(f"Source {source} is not supported.")
