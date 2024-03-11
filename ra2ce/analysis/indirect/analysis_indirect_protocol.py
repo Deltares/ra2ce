641e38af6e6baf161a1732ddb65a55c9ccb469e5
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-03-11 11:23:35 +0100

    chore: add indirect protocl

---
+++ ra2ce/analysis/indirect/analysis_indirect_protocol.py
@@ -0,0 +1,46 @@
+"""
+                    GNU GENERAL PUBLIC LICENSE
+                      Version 3, 29 June 2007
+
+    Risk Assessment and Adaptation for Critical Infrastructure (RA2CE).
+    Copyright (C) 2023 Stichting Deltares
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from pathlib import Path
+from typing import Optional
+
+from geopandas import GeoDataFrame
+
+from ra2ce.analysis.analysis_config_data.analysis_config_data import (
+    AnalysisSectionIndirect,
+)
+from ra2ce.analysis.analysis_protocol import AnalysisProtocol
+from ra2ce.network.graph_files.network_file import NetworkFile
+
+
+class AnalysisIndirectProtocol(AnalysisProtocol):
+    graph_file: NetworkFile
+    analysis: AnalysisSectionIndirect
+    input_path: Path
+    output_path: Path
+    result: Optional[GeoDataFrame]
+
+    def execute(self) -> GeoDataFrame:  # TODO make it a result object
+        """
+        Execute the analysis on the given graph/network with the given analysis parameters.
+        The resulting (Geo)DataFrame of the analysis is stored in the result attribute.
+        """
+        pass
