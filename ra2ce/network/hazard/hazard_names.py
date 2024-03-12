a292cd90e427ee22f5c846bde8c6517fee263c57
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-03-12 10:33:15 +0100

    chore: add docstring to hazard_names

--- ra2ce/network/hazard/hazard_names.py
+++ ra2ce/network/hazard/hazard_names.py
@@ -1,3 +1,26 @@
+"""
+                    GNU GENERAL PUBLIC LICENSE
+                      Version 3, 29 June 2007
+
+    Risk Assessment and Adaptation for Critical Infrastructure (RA2CE).
+    Copyright (C) 2023 Stichting Deltares
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from __future__ import annotations
+
 from pathlib import Path
 
 import pandas as pd
@@ -15,7 +38,16 @@ class HazardNames:
         self.names = hazard_names
 
     @classmethod
-    def from_file(cls, hazard_names_file: Path):
+    def from_file(cls, hazard_names_file: Path) -> HazardNames:
+        """
+        Create a HazardNames object from a file.
+
+        Args:
+            hazard_names_file (Path): Path to the files with the hazard names.
+
+        Returns:
+            HazardNames: HazardNames object.
+        """
         if hazard_names_file.is_file():
             _names_df = pd.read_excel(hazard_names_file)
             _names = _names_df[FILE_NAME_KEY].tolist()
@@ -25,6 +57,15 @@ class HazardNames:
         return cls(_names_df, _names)
 
     def get_name(self, hazard: str) -> str:
+        """
+        Get the RA2CE name of a specific hazard.
+
+        Args:
+            hazard (str): Name of the hazard.
+
+        Returns:
+            str: RA2CE name of the hazard.
+        """
         return self.names_df.loc[
             self.names_df[FILE_NAME_KEY] == hazard,
             RA2CE_NAME_KEY,
