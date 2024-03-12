11d7624a3e18d48e607fc51fb06e39fe6ec2cafc
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-03-12 10:30:46 +0100

    chore: add docstring and test for hazard_files

--- ra2ce/network/hazard/hazard_files.py
+++ ra2ce/network/hazard/hazard_files.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
 from pathlib import Path
 
@@ -9,7 +11,17 @@ class HazardFiles:
     table: list[Path] = field(default_factory=list)
 
     @classmethod
-    def from_hazard_map(cls, hazard_map: list[Path]):
+    def from_hazard_map(cls, hazard_map: list[Path]) -> HazardFiles:
+        """
+        Create a HazardFiles object from a list of hazard map files.
+
+        Args:
+            hazard_map (list[Path]): _description_
+
+        Returns:
+            HazardFiles: _description_
+        """
+
         def _get_filtered_files(*suffix) -> list[Path]:
             _filter = lambda x: x.suffix in list(suffix)
             return list(filter(_filter, hazard_map))
