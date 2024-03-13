f11c72cf9707cb33570b378d94be38208c2ce0e6
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-03-13 17:06:56 +0100

    chore: add new enums

---
+++ ra2ce/analysis/analysis_config_data/enums/event_type_enum.py
@@ -0,0 +1,7 @@
+from ra2ce.configuration.ra2ce_enum_base import Ra2ceEnumBase
+
+
+class EventTypeEnum(Ra2ceEnumBase):
+    EVENT = 0
+    RETURN_PERIOD = 1
+    INVALID = 99
---
+++ ra2ce/analysis/analysis_config_data/enums/loss_type_enum.py
@@ -0,0 +1,7 @@
+from ra2ce.configuration.ra2ce_enum_base import Ra2ceEnumBase
+
+
+class LossTypeEnum(Ra2ceEnumBase):
+    UNIFORM = 0
+    CATEGORIZED = 1
+    INVALID = 99
