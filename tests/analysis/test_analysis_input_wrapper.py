ff1a87045a3a92a5d0ef306c8b3f76de3e44e24d
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-03-19 11:57:03 +0100

    chore: create analysis_input_wrapper

--- tests/analysis/test_analysis_input_wrapper.py
+++ tests/analysis/test_analysis_input_wrapper.py
@@ -1,9 +1,9 @@
 from dataclasses import dataclass
-from pathlib import Path
 
 from ra2ce.analysis.analysis_config_data.analysis_config_data import AnalysisSectionBase
 from ra2ce.analysis.analysis_config_wrapper import AnalysisConfigWrapper
 from ra2ce.analysis.analysis_input_wrapper import AnalysisInputWrapper
+from ra2ce.network.graph_files.graph_file import GraphFile
 
 
 class TestAnalysisInputWrapper:
@@ -16,10 +16,16 @@ class TestAnalysisInputWrapper:
         # 1. Define test data
         _analysis = self.MockAnalysisSection()
         _config = AnalysisConfigWrapper()
-        _config.config_data.output_path = Path("just a path")
+        _graph_file = GraphFile(name="test_graph_file")
+        _graph_file_hazard = GraphFile(name="test_graph_file_hazard")
 
         # 2. Run test
-        _analysis_input = AnalysisInputWrapper(_analysis, _config)
+        _analysis_input = AnalysisInputWrapper(
+            _analysis, _config, _graph_file, _graph_file_hazard
+        )
 
         # 3. Verify expectations
         assert isinstance(_analysis_input, AnalysisInputWrapper)
+        assert _analysis_input.analysis == _analysis
+        assert _analysis_input.graph_file == _graph_file
+        assert _analysis_input.graph_file_hazard == _graph_file_hazard
