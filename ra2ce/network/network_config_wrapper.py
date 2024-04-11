6fcdcfe398fc47c5227d471bb5ebc47be8da9c34
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2024-04-11 13:57:01 +0200

    fix: multi link losses document missing test data. (#415)
    * chore: Modified `NetworkConfigWrapper.is_valid()` and `AnalysisConfigWrapper.,is_valid()`, so that they no longer require an `ini` file to be present.
    
    * docs: Fixed `example_multi_link_losses.ipynb`
    
    * docs: Added multi_link_losses to the documentation index. Added test data for multi_link_losses. Moved results from hackathon to proper directory.
    
    * docs: Adding the two required files to multi link losses
    
    * test: Added missing tests for the config wrapper validations
    
    ---------
    
    Co-authored-by: LiekeMeijer <lieke.meijer@deltares.nl>

--- ra2ce/network/network_config_wrapper.py
+++ ra2ce/network/network_config_wrapper.py
@@ -103,6 +103,4 @@ class NetworkConfigWrapper(ConfigWrapperProtocol):
         self.graph_files = hazard.create()
 
     def is_valid(self) -> bool:
-        _file_is_valid = self.ini_file.is_file() and self.ini_file.suffix == ".ini"
-        _validation_report = NetworkConfigDataValidator(self.config_data).validate()
-        return _file_is_valid and _validation_report.is_valid()
+        return NetworkConfigDataValidator(self.config_data).validate().is_valid()
