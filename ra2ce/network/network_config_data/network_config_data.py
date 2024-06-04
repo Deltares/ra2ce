7d8b016647d0355d660d0066ed825849c272e60d
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-06-04 15:14:30 +0200

    docs: add documentation of the new setting

--- ra2ce/network/network_config_data/network_config_data.py
+++ ra2ce/network/network_config_data/network_config_data.py
@@ -82,6 +82,8 @@ class HazardSection:
     aggregate_wl: AggregateWlEnum = field(default_factory=lambda: AggregateWlEnum.NONE)
     hazard_crs: str = ""
     scenario_cost: list[float] = field(default_factory=list)
+    # Should a hazard overlay be created for the segmented network.
+    # This is not needed for most analyses and will speed up the configuration of the network.
     overlay_segmented_network: bool = True
 
 
