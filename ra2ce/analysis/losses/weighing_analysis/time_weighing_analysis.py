30a0b7f08f3065fe95974bce4289f2c2b11595ea
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-06-10 13:40:52 +0200

    chore: make time_weighing_analysis more robust with default avgspeed = inf

--- ra2ce/analysis/losses/weighing_analysis/time_weighing_analysis.py
+++ ra2ce/analysis/losses/weighing_analysis/time_weighing_analysis.py
@@ -11,22 +11,22 @@ from ra2ce.analysis.losses.weighing_analysis.weighing_analysis_protocol import (
 class TimeWeighingAnalysis(WeighingAnalysisProtocol):
     time_list: list
     weighing_data: dict
+    default_avgspeed: float = math.inf
 
     def __init__(self) -> None:
         self.time_list = []
 
     def _calculate_time(self) -> float:
-        length = self.weighing_data.get("length", None)
+        length = self.weighing_data.get("length", 0)
         avgspeed = self.weighing_data.get("avgspeed", None)
-        if length and avgspeed:
-            _calculated_time = round(
-                (length * 1e-3) / avgspeed,
-                3,
-            )  # in hours and avg speed in km/h
-            self.weighing_data[WeighingEnum.TIME.config_value] = _calculated_time
-            return round(_calculated_time, 3)
-        else:
-            return math.nan
+        if not avgspeed:
+            avgspeed = self.default_avgspeed
+        _calculated_time = round(
+            (length * 1e-3) / avgspeed,
+            3,
+        )  # in hours and avg speed in km/h
+        self.weighing_data[WeighingEnum.TIME.config_value] = _calculated_time
+        return round(_calculated_time, 3)
 
     def calculate_value(self) -> float:
         self.time_list.append(self._calculate_time())
@@ -34,12 +34,11 @@ class TimeWeighingAnalysis(WeighingAnalysisProtocol):
 
     def calculate_alternative_value(self, alt_dist: float) -> float:
         avgspeed = self.weighing_data.get("avgspeed", None)
-        if avgspeed:
-            alt_time = (alt_dist * 1e-3) / avgspeed  # in hours
-            self.time_list.append(self._calculate_time())
-            return alt_time
-        else:
-            return math.nan
+        if not avgspeed:
+            avgspeed = self.default_avgspeed
+        alt_time = (alt_dist * 1e-3) / avgspeed  # in hours
+        self.time_list.append(self._calculate_time())
+        return alt_time
 
     def extend_graph(self, gdf_graph: gpd.GeoDataFrame | dict) -> None:
         if isinstance(gdf_graph, gpd.GeoDataFrame):
