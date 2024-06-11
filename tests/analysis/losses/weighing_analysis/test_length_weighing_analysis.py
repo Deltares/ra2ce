chore: make gdf_graph optional on init; fix bug on distance calculation; fix tests

tests/analysis/losses/weighing_analysis/test_length_weighing_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+from ra2ce.analysis.analysis_config_data.enums.weighing_enum import WeighingEnum
 from ra2ce.analysis.losses.weighing_analysis.length_weighing_analysis import (
     LengthWeighingAnalysis,
 )
@@ -12,7 +13,7 @@ from ra2ce.analysis.losses.weighing_analysis.weighing_analysis_protocol import (
 class TestLengthWeighingAnalysis:
     def test_init(self):
         # 1. Define test data.
-        _analysis = LengthWeighingAnalysis()
+        _analysis = LengthWeighingAnalysis(None)
 
         # 2. Verify expectations.
         assert isinstance(_analysis, LengthWeighingAnalysis)
@@ -20,18 +21,37 @@ class TestLengthWeighingAnalysis:
 
     @pytest.fixture
     def valid_analysis(self) -> LengthWeighingAnalysis:
-        return LengthWeighingAnalysis()
+        _analysis = LengthWeighingAnalysis(None)
+        _analysis.edge_data = {WeighingEnum.TIME.config_value: 1, "avgspeed": 0.42}
+        return _analysis
 
     def test_calculate_distance(self, valid_analysis: LengthWeighingAnalysis):
-        # 1. Run test
-        _calculated_distance = valid_analysis.get_current_value()
+        # 1. Define test data.
+        _expected_distance = 420.0
+        _time = valid_analysis.edge_data[WeighingEnum.TIME.config_value]
 
-        # 2. Verify expectations.
-        assert np.isnan(_calculated_distance)
+        # 2. Run test
+        _calculated_distance = valid_analysis._calculate_distance(_time)
 
-    def test_calculate_alternative_distance(
-        self, valid_analysis: LengthWeighingAnalysis
-    ):
+        # 3. Verify expectations.
+        assert _calculated_distance == pytest.approx(_expected_distance)
+
+    def test_get_current_value(self, valid_analysis: LengthWeighingAnalysis):
+        # 1. Define test data.
+        _expected_distance = 420.0
+        _dist = valid_analysis.edge_data.get(WeighingEnum.LENGTH.config_value, 0)
+        assert not _dist
+
+        # 2. Run test
+        _current_distance = valid_analysis.get_current_value()
+
+        # 3. Verify expectations.
+        assert _current_distance == pytest.approx(_expected_distance)
+        assert valid_analysis.edge_data[
+            WeighingEnum.LENGTH.config_value
+        ] == pytest.approx(_expected_distance)
+
+    def test_calculate_alternative_value(self, valid_analysis: LengthWeighingAnalysis):
         # 1. Define test data.
         _alt_distance = 42
 
@@ -40,14 +60,3 @@ class TestLengthWeighingAnalysis:
 
         # 2. Verify expectations.
         assert _calculated_distance == _alt_distance
-
-    def test_extend_graph(self, valid_analysis: LengthWeighingAnalysis):
-        # 1. Define test data.
-        _graph_dict = dict(my_value=42)
-
-        # 2. Run test.
-        valid_analysis.extend_graph(_graph_dict)
-
-        # 3. Verify expectations
-        assert len(_graph_dict) == 1
-        assert _graph_dict["my_value"] == 42