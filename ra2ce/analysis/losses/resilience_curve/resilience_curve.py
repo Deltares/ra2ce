7e28a57947451c3d5138620f01d1c889d7865d9b
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-06-27 10:30:13 +0200

    chore: expand dataclass and test

--- ra2ce/analysis/losses/resilience_curve/resilience_curve.py
+++ ra2ce/analysis/losses/resilience_curve/resilience_curve.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass, field
 
+import numpy as np
+
 from ra2ce.network.network_config_data.enums.road_type_enum import RoadTypeEnum
 
 
@@ -11,8 +13,26 @@ class ResilienceCurve:
     duration_steps: list[list[float]] = field(default_factory=list)
     functionality_loss_ratio: list[list[float]] = field(default_factory=list)
 
-    @property
-    def disruption(self) -> float:
+    def _get_index(self, link_type: RoadTypeEnum, hazard_min: float) -> int:
+        _link_type_indices = np.where(np.array(self.link_type) == link_type)[0]
+        _hazard_min_indices = np.where(np.array(self.hazard_min) == hazard_min)[0]
+        return int(np.intersect1d(_link_type_indices, _hazard_min_indices)[0])
+
+    def get_duration_steps(
+        self, link_type: RoadTypeEnum, hazard_min: float
+    ) -> list[float]:
+        return self.duration_steps[self._get_index(link_type, hazard_min)]
+
+    def get_functionality_loss_ratio(
+        self, link_type: RoadTypeEnum, hazard_min: float
+    ) -> list[float]:
+        return self.functionality_loss_ratio[self._get_index(link_type, hazard_min)]
+
+    def get_disruption(self, link_type: RoadTypeEnum, hazard_min: float) -> float:
         return sum(
-            map(lambda x, y: x * y, self.duration_steps, self.functionality_loss_ratio)
+            map(
+                lambda x, y: x * y,
+                self.get_duration_steps(link_type, hazard_min),
+                self.get_functionality_loss_ratio(link_type, hazard_min),
+            )
         )
