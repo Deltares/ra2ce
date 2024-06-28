b6bbcd91b2b2e8b32e18383b502f5b2c4bf77ad5
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-06-28 11:54:00 +0200

    chore:         network_simplification_with_attribute_exclusion: NetworkSimplificationWithAttributeExclusion,

--- tests/network/network_simplification/test_network_graph_simplificator.py
+++ tests/network/network_simplification/test_network_graph_simplificator.py
@@ -1,5 +1,5 @@
 import random
-from typing import Iterator
+from typing import Callable, Iterator
 
 import networkx as nx
 import numpy as np
@@ -37,53 +37,71 @@ def _detailed_edge_comparison(
 
 
 class TestNetworkGraphSimplificator:
-    @pytest.fixture(name="network_graph_simplificator")
+    @pytest.fixture(name="network_graph_simplificator_factory")
     def _get_network_graph_simplificator(
         self,
-    ) -> Iterator[NetworkGraphSimplificator]:
-        yield NetworkGraphSimplificator(graph_complex=None, attributes_to_exclude=[])
+    ) -> Iterator[Callable[[], NetworkGraphSimplificator]]:
+        def get_network_graph_simplificator() -> NetworkGraphSimplificator:
+            return NetworkGraphSimplificator(
+                graph_complex=None, attributes_to_exclude=[]
+            )
+
+        yield get_network_graph_simplificator
 
     def test_validate_fixture_init(
         self,
-        network_graph_simplificator: NetworkGraphSimplificator,
+        network_graph_simplificator_factory: Callable[[], NetworkGraphSimplificator],
     ):
-        assert isinstance(network_graph_simplificator, NetworkGraphSimplificator)
+        # 1. Define test data.
+        _network_graph_simplificator = network_graph_simplificator_factory()
 
-    def test__graph_create_unique_ids_with_missing_id_data(
-        self, network_graph_simplificator: NetworkGraphSimplificator
-    ):
-        # 1. Define test data
+        # 2. Verify expectations
+        assert isinstance(_network_graph_simplificator, NetworkGraphSimplificator)
+
+    @pytest.fixture(name="multigraph_fixture")
+    def _get_multigraph_fixture(self) -> Iterator[nx.MultiGraph]:
         _graph = nx.MultiGraph()
         _graph.add_edge(2, 3, weight=5)
         _graph.add_edge(2, 1, weight=2)
+        yield _graph
+
+    def test__graph_create_unique_ids_with_missing_id_data(
+        self,
+        network_graph_simplificator_factory: Callable[[], NetworkGraphSimplificator],
+        multigraph_fixture: nx.MultiGraph,
+    ):
+        # 1. Define test data
+        _network_graph_simplificator = network_graph_simplificator_factory()
+        assert isinstance(multigraph_fixture, nx.MultiGraph)
         _new_id_name = "dummy_id"
 
         # 2. Run test
-        _return_graph = network_graph_simplificator._graph_create_unique_ids(
-            _graph, _new_id_name
+        _return_graph = _network_graph_simplificator._graph_create_unique_ids(
+            multigraph_fixture, _new_id_name
         )
 
         # 3. Verify final expectations
-        assert _return_graph == _graph
-        _dicts_keys = [_k[-1].keys() for _k in _graph.edges.data(keys=True)]
+        assert _return_graph == multigraph_fixture
+        _dicts_keys = [_k[-1].keys() for _k in multigraph_fixture.edges.data(keys=True)]
         assert all(_new_id_name in _keys for _keys in _dicts_keys)
 
     def test__graph_create_unique_ids_with_existing_id(
-        self, network_graph_simplificator: NetworkGraphSimplificator
+        self,
+        network_graph_simplificator_factory: NetworkGraphSimplificator,
+        multigraph_fixture: nx.MultiGraph,
     ):
         # 1. Define test data
-        _graph = nx.MultiGraph()
-        _graph.add_edge(2, 3, weight=5)
-        _graph.add_edge(2, 1, weight=2)
+        _network_graph_simplificator = network_graph_simplificator_factory()
+        assert isinstance(multigraph_fixture, nx.MultiGraph)
         _new_id_name = "weight"
 
         # 2. Run test
-        _return_graph = network_graph_simplificator._graph_create_unique_ids(
-            _graph, _new_id_name
+        _return_graph = _network_graph_simplificator._graph_create_unique_ids(
+            multigraph_fixture, _new_id_name
         )
 
         # 3. Verify final expectations
-        assert _return_graph == _graph
+        assert _return_graph == multigraph_fixture
 
 
 class TestNetworkSimplificationWithAttributeExclusion:
@@ -95,40 +113,46 @@ class TestNetworkSimplificationWithAttributeExclusion:
             nx_graph=None, attributes_to_exclude=[]
         )
 
-    @pytest.fixture(name="_nx_digraph")
-    def _nx_digraph_fixture(self) -> nx.MultiDiGraph:
-        _nx_digraph = nx.MultiDiGraph()
-        for i in range(1, 16):
-            random_x = random.randint(0, 5) * random.choice([-1, 1])
-            random_y = random.randint(0, 5) * random.choice([-1, 1])
-            _nx_digraph.add_node(i, x=random_x, y=random_y)
+    @pytest.fixture(name="nx_digraph_factory")
+    def _get_nx_digraph_factory(self) -> Iterator[Callable[[], nx.MultiDiGraph]]:
+        def create_nx_multidigraph():
+            _nx_digraph = nx.MultiDiGraph()
+            for i in range(1, 16):
+                random_x = random.randint(0, 5) * random.choice([-1, 1])
+                random_y = random.randint(0, 5) * random.choice([-1, 1])
+                _nx_digraph.add_node(i, x=random_x, y=random_y)
 
-        _nx_digraph.add_edge(1, 2, a=np.nan)
-        _nx_digraph.add_edge(2, 1, a=np.nan)
-        _nx_digraph.add_edge(2, 3, a=np.nan)
-        _nx_digraph.add_edge(3, 4, a=np.nan)
-        _nx_digraph.add_edge(4, 5, a="yes")
-        _nx_digraph.add_edge(5, 6, a="yes")
-        _nx_digraph.add_edge(6, 7, a="yes")
-        _nx_digraph.add_edge(7, 8, a=np.nan)
-        _nx_digraph.add_edge(8, 9, a=np.nan)
-        _nx_digraph.add_edge(8, 12, a=np.nan)
-        _nx_digraph.add_edge(8, 13, a="yes")
-        _nx_digraph.add_edge(9, 10, a=np.nan)
-        _nx_digraph.add_edge(10, 11, a=np.nan)
-        _nx_digraph.add_edge(11, 12, a="yes")
-        _nx_digraph.add_edge(13, 14, a="yes")
-        _nx_digraph.add_edge(14, 15, a="yes")
-        _nx_digraph.add_edge(15, 11, a="yes")
+            _nx_digraph.add_edge(1, 2, a=np.nan)
+            _nx_digraph.add_edge(2, 1, a=np.nan)
+            _nx_digraph.add_edge(2, 3, a=np.nan)
+            _nx_digraph.add_edge(3, 4, a=np.nan)
+            _nx_digraph.add_edge(4, 5, a="yes")
+            _nx_digraph.add_edge(5, 6, a="yes")
+            _nx_digraph.add_edge(6, 7, a="yes")
+            _nx_digraph.add_edge(7, 8, a=np.nan)
+            _nx_digraph.add_edge(8, 9, a=np.nan)
+            _nx_digraph.add_edge(8, 12, a=np.nan)
+            _nx_digraph.add_edge(8, 13, a="yes")
+            _nx_digraph.add_edge(9, 10, a=np.nan)
+            _nx_digraph.add_edge(10, 11, a=np.nan)
+            _nx_digraph.add_edge(11, 12, a="yes")
+            _nx_digraph.add_edge(13, 14, a="yes")
+            _nx_digraph.add_edge(14, 15, a="yes")
+            _nx_digraph.add_edge(15, 11, a="yes")
 
-        _nx_digraph = add_missing_geoms_graph(_nx_digraph, "geometry")
-        _nx_digraph.graph["crs"] = "EPSG:4326"
+            _nx_digraph = add_missing_geoms_graph(_nx_digraph, "geometry")
+            _nx_digraph.graph["crs"] = "EPSG:4326"
 
-        _nx_digraph = add_missing_geoms_graph(_nx_digraph, "geometry")
-        return _nx_digraph
+            _nx_digraph = add_missing_geoms_graph(_nx_digraph, "geometry")
+            return _nx_digraph
 
-    @pytest.fixture(name="_result_graph")
-    def _result_graph_fixture(self, _nx_digraph: nx.MultiDiGraph) -> nx.MultiGraph:
+        yield create_nx_multidigraph
+
+    @pytest.fixture(name="expected_result_graph_fixture")
+    def _get_expected_result_graph_fixture(
+        self, nx_digraph_factory: nx.MultiDiGraph
+    ) -> nx.MultiGraph:
+        _nx_digraph = nx_digraph_factory()
         _result_digraph = nx.MultiGraph()
         node_ids_degrees = {2: 1, 4: 2, 7: 2, 8: 4, 11: 3, 12: 2}
         for node_id, degree in node_ids_degrees.items():
@@ -254,18 +278,20 @@ class TestNetworkSimplificationWithAttributeExclusion:
 
     def test_simplify_graph(
         self,
-        _nx_digraph: nx.MultiDiGraph,
-        _result_graph: nx.MultiDiGraph,
         network_simplification_with_attribute_exclusion: NetworkSimplificationWithAttributeExclusion,
+        nx_digraph_factory: Callable[[], nx.MultiDiGraph],
+        expected_result_graph_fixture: nx.MultiDiGraph,
     ):
-        network_simplification_with_attribute_exclusion.nx_graph = _nx_digraph
+        network_simplification_with_attribute_exclusion.nx_graph = nx_digraph_factory()
         network_simplification_with_attribute_exclusion.attributes_to_exclude = ["a"]
 
         _graph_simple = network_simplification_with_attribute_exclusion.simplify_graph()
 
         # Compare nodes with attributes
-        assert _graph_simple.nodes(data=True) == _result_graph.nodes(data=True)
+        assert _graph_simple.nodes(data=True) == expected_result_graph_fixture.nodes(
+            data=True
+        )
         # Compare edges topology
-        assert set(_graph_simple.edges()) == set(_result_graph.edges())
+        assert set(_graph_simple.edges()) == set(expected_result_graph_fixture.edges())
         # Compare edges with attributes
-        assert _detailed_edge_comparison(_graph_simple, _result_graph)
+        assert _detailed_edge_comparison(_graph_simple, expected_result_graph_fixture)
