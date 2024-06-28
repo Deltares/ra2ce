0442ce43094f8f90aa21b9ea7b6ff7d6eaf60f22
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2024-06-28 08:10:15 +0200

    chore: add get_enum to enums to avoid linting issues

--- ra2ce/network/network_config_data/enums/part_of_day_enum.py
+++ ra2ce/network/network_config_data/enums/part_of_day_enum.py
@@ -1,6 +1,13 @@
+from __future__ import annotations
+
 from ra2ce.configuration.ra2ce_enum_base import Ra2ceEnumBase
 
 
 class PartOfDayEnum(Ra2ceEnumBase):
-    DAY = 0
-    EVENING = 1
+    DAY = 1
+    EVENING = 2
+    INVALID = 99
+
+    @classmethod
+    def get_enum(cls, input: str | None) -> PartOfDayEnum:
+        return super().get_enum(input)
