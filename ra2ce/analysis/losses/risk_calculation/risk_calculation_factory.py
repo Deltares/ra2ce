9a4b68558277d9f9d6345f6d22f740b86a69d4a4
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2024-07-16 09:05:57 +0200

    feat: risk_calculation_factory returns the correct RiskCalculationBase classes

--- ra2ce/analysis/losses/risk_calculation/risk_calculation_factory.py
+++ ra2ce/analysis/losses/risk_calculation/risk_calculation_factory.py
@@ -2,6 +2,15 @@ import geopandas as gpd
 from ra2ce.analysis.analysis_config_data.enums.risk_calculation_mode_enum import (
     RiskCalculationModeEnum,
 )
+from ra2ce.analysis.losses.risk_calculation.risk_calculation_cut_from_year import (
+    RiskCalculationCutFromYear,
+)
+from ra2ce.analysis.losses.risk_calculation.risk_calculation_default import (
+    RiskCalculationDefault,
+)
+from ra2ce.analysis.losses.risk_calculation.risk_calculation_triangle_to_null_year import (
+    RiskCalculationTriangleToNullYear,
+)
 
 
 class RiskCalculationFactory:
@@ -12,11 +21,11 @@ class RiskCalculationFactory:
         losses_gdf: gpd.GeoDataFrame,
     ):
         if risk_calculation_mode == RiskCalculationModeEnum.DEFAULT:
-            pass
+            return RiskCalculationDefault(risk_calculation_year, losses_gdf)
         if risk_calculation_mode == RiskCalculationModeEnum.CUT_FROM_YEAR:
-            pass
+            return RiskCalculationCutFromYear(risk_calculation_year, losses_gdf)
         if risk_calculation_mode == RiskCalculationModeEnum.TRIANGLE_TO_NULL_YEAR:
-            pass
+            return RiskCalculationTriangleToNullYear(risk_calculation_year, losses_gdf)
         raise NotImplementedError(
             "Risk calculation mode {} not yet supported.".format(risk_calculation_mode)
         )
