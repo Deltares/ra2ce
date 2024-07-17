49e67b951ab5e6002927eaa9ffd8bd6522688628
Carles S. Soriano Perez <carles.sorianoperez@deltares.nl>
2024-07-17 15:31:34 +0200

    ci: Added auxiliar scripts

---
+++ examples/poc-uncertainties/workflows/collect_data.py
@@ -0,0 +1,22 @@
+import json
+from pathlib import Path
+
+import boto3
+
+# s3://ra2ce-data/uncertainty_hackathon/flood_maps/
+bucket = "ra2ce-data"
+_root_dir = "uncertainty_hackathon/flood_maps/"
+
+client = boto3.client("s3")
+result = client.list_objects(Bucket=bucket, Prefix=_root_dir, Delimiter="/")
+members = []
+for _sub_prefix in result.get("CommonPrefixes"):
+    _prefix = _sub_prefix["Prefix"]
+    _prefix_path = Path(_prefix)
+    _prefix_result = client.list_objects(Bucket=bucket, Prefix=_prefix, Delimiter="/")
+    _event_files = [
+        str(Path(_c["Key"]).relative_to(_prefix_path.parent))
+        for _c in _prefix_result.get("Contents")
+    ]
+    members.extend(_event_files)
+print(json.dumps(members))
