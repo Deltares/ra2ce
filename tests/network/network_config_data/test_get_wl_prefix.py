8bb32cca6de7c7d2ebdaa1a06c42c4bd9c2533e9
sahand_asgarpour <sahand.asgarpour@deltares.nl>
2024-07-24 17:11:33 +0200

    test: get_wl _prefix is added

---
+++ tests/network/network_config_data/test_get_wl_prefix.py
@@ -0,0 +1,33 @@
+import pytest
+
+from ra2ce.network.network_config_data.enums.aggregate_wl_enum import AggregateWlEnum
+
+
+class TestGetWlPrefix:
+    @pytest.mark.parametrize(
+        "aggregate_wl",
+        "expected_prefix",
+        [
+            pytest.param(
+                AggregateWlEnum.MAX,
+                "ma",
+                id=AggregateWlEnum.MAX.name.lower(),
+            ),
+            pytest.param(
+                AggregateWlEnum.MAX,
+                id=AggregateWlEnum.MIN.name.lower(),
+            ),
+            pytest.param(
+                AggregateWlEnum.MEAN,
+                id=AggregateWlEnum.MAX.name.lower(),
+            ),
+        ],
+    )
+    def test_aggregate_wl_prefix(
+        self, aggregate_wl: AggregateWlEnum, expected_prefix: str
+    ):
+        # 1./2. Define test data / Run test
+        _prefix = aggregate_wl.get_wl_prefix()
+
+        # 3. Verify results
+        assert _prefix == expected_prefix
