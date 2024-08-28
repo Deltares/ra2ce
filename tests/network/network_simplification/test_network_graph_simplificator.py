6c19b6386b6520eeaa81ca097d523e1b57f41a29
asgarpou <sahand.asgarpour@deltares.nl>
2024-08-28 23:42:00 +0200

    chore: test updated

--- tests/network/network_simplification/test_network_graph_simplificator.py
+++ tests/network/network_simplification/test_network_graph_simplificator.py
@@ -26,19 +26,27 @@ def _detailed_edge_comparison(
     graph1: nx.MultiDiGraph | nx.MultiGraph, graph2: nx.MultiDiGraph | nx.MultiGraph
 ) -> bool:
     def _dicts_comparison(
-        graph1: nx.MultiDiGraph | nx.MultiGraph, graph2: nx.MultiDiGraph | nx.MultiGraph
+        _graph1: nx.MultiDiGraph | nx.MultiGraph, _graph2: nx.MultiDiGraph | nx.MultiGraph
     ) -> bool:
-        for u, v, k, data1 in graph1.edges(keys=True, data=True):
-            data2 = graph2.get_edge_data(u, v, k)
-            for key1, value1 in data1.items():
-                if key1 not in data2:
-                    return False
-                if isinstance(value1, float) and math.isnan(value1):
-                    if not math.isnan(data2[key1]):
-                        return False
-                    continue
-                if value1 != data2[key1]:
-                    return False
+        for u, v, k, data1 in _graph1.edges(keys=True, data=True):
+            geom1 = data1['geometry']
+            geom_found = 0
+
+            data2_dict = _graph2.get_edge_data(u, v)
+            for _, data2 in data2_dict.items():
+                if data2['geometry'] == geom1:
+                    geom_found = 1
+                    for key1, value1 in data1.items():
+                        if key1 not in data2:
+                            return False
+                        if isinstance(value1, float) and math.isnan(value1):
+                            if not math.isnan(data2[key1]):
+                                return False
+                            continue
+                        if value1 != data2[key1]:
+                            return False
+            if geom_found == 0:
+                return False
         return True
 
     check_1_2 = _dicts_comparison(graph1, graph2)
@@ -134,27 +142,27 @@ class TestNetworkSimplificationWithAttributeExclusion:
             for i in range(1, 19):
                 _nx_digraph.add_node(i, x=i, y=i * 10)
 
-            _nx_digraph.add_edge(1, 2, a=np.nan)
-            _nx_digraph.add_edge(2, 1, a=np.nan)
-            _nx_digraph.add_edge(2, 3, a=np.nan)
-            _nx_digraph.add_edge(3, 4, a=np.nan)
+            _nx_digraph.add_edge(1, 2, a='None')
+            _nx_digraph.add_edge(2, 1, a='None')
+            _nx_digraph.add_edge(2, 3, a='None')
+            _nx_digraph.add_edge(3, 4, a='None')
             _nx_digraph.add_edge(4, 5, a="yes")
             _nx_digraph.add_edge(5, 6, a="yes")
             _nx_digraph.add_edge(6, 7, a="yes")
-            _nx_digraph.add_edge(7, 8, a=np.nan)
-            _nx_digraph.add_edge(8, 9, a=np.nan)
-            _nx_digraph.add_edge(8, 12, a=np.nan)
+            _nx_digraph.add_edge(7, 8, a='None')
+            _nx_digraph.add_edge(8, 9, a='None')
+            _nx_digraph.add_edge(8, 12, a='None')
             _nx_digraph.add_edge(8, 13, a="yes")
-            _nx_digraph.add_edge(9, 10, a=np.nan)
-            _nx_digraph.add_edge(10, 11, a=np.nan)
+            _nx_digraph.add_edge(9, 10, a='None')
+            _nx_digraph.add_edge(10, 11, a='None')
             _nx_digraph.add_edge(11, 12, a="yes")
             _nx_digraph.add_edge(13, 14, a="yes")
             _nx_digraph.add_edge(14, 15, a="yes")
             _nx_digraph.add_edge(15, 11, a="yes")
-            _nx_digraph.add_edge(1, 16, a=np.nan)
-            _nx_digraph.add_edge(16, 1, a=np.nan)
-            _nx_digraph.add_edge(16, 17, a=np.nan)
-            _nx_digraph.add_edge(16, 18, a=np.nan)
+            _nx_digraph.add_edge(1, 16, a='None')
+            _nx_digraph.add_edge(16, 1, a='None')
+            _nx_digraph.add_edge(16, 17, a='None')
+            _nx_digraph.add_edge(16, 18, a='None')
 
             _nx_digraph = add_missing_geoms_graph(_nx_digraph, "geometry")
             _nx_digraph.graph["crs"] = "EPSG:4326"
@@ -169,7 +177,7 @@ class TestNetworkSimplificationWithAttributeExclusion:
         self, nx_digraph_factory: nx.MultiDiGraph
     ) -> nx.MultiGraph:
         _nx_digraph = nx_digraph_factory()
-        _result_digraph = nx.MultiGraph()
+        _result_digraph = nx.MultiDiGraph()
         node_ids_degrees = {2: 3, 4: 2, 7: 2, 8: 4, 11: 3, 12: 2, 16: 4, 17: 1, 18: 1}
         for node_id, degree in node_ids_degrees.items():
             node_data = _nx_digraph.nodes[node_id]
@@ -181,7 +189,7 @@ class TestNetworkSimplificationWithAttributeExclusion:
         _result_digraph.add_edge(
             2,
             4.0,
-            a=np.nan,
+            a='None',
             from_node=2,
             to_node=4,
             geometry=LineString(
@@ -196,7 +204,7 @@ class TestNetworkSimplificationWithAttributeExclusion:
         _result_digraph.add_edge(
             2,
             16.0,
-            a=np.nan,
+            a='None',
             from_node=2,
             to_node=16,
             geometry=LineString(
@@ -226,7 +234,7 @@ class TestNetworkSimplificationWithAttributeExclusion:
         _result_digraph.add_edge(
             7,
             8.0,
-            a=np.nan,
+            a='None',
             from_node=7,
             to_node=8,
             geometry=LineString(
@@ -239,7 +247,7 @@ class TestNetworkSimplificationWithAttributeExclusion:
         _result_digraph.add_edge(
             8,
             11,
-            a=np.nan,
+            a='None',
             from_node=8,
             to_node=11,
             geometry=LineString(
@@ -270,7 +278,7 @@ class TestNetworkSimplificationWithAttributeExclusion:
         _result_digraph.add_edge(
             8,
             12,
-            a=np.nan,
+            a='None',
             from_node=8,
             to_node=12,
             geometry=LineString(
@@ -297,7 +305,7 @@ class TestNetworkSimplificationWithAttributeExclusion:
         _result_digraph.add_edge(
             16,
             2.0,
-            a=np.nan,
+            a='None',
             from_node=16,
             to_node=2,
             geometry=LineString(
@@ -312,7 +320,7 @@ class TestNetworkSimplificationWithAttributeExclusion:
         _result_digraph.add_edge(
             16,
             17,
-            a=np.nan,
+            a='None',
             from_node=16,
             to_node=17,
             geometry=LineString(
@@ -326,7 +334,7 @@ class TestNetworkSimplificationWithAttributeExclusion:
         _result_digraph.add_edge(
             16,
             18,
-            a=np.nan,
+            a='None',
             from_node=16,
             to_node=18,
             geometry=LineString(
