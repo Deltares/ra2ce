876f8e60405132501c2234ce7237c6eda10506b9
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2024-12-04 11:55:57 +0100

    chore: fixed wrong imports

--- ra2ce/analysis/analysis_protocol.py
+++ ra2ce/analysis/analysis_protocol.py
@@ -23,7 +23,7 @@ from pathlib import Path
 from typing import Optional, Protocol
 
 from ra2ce.analysis.analysis_config_data.analysis_config_data import AnalysisSectionBase
-from ra2ce.analysis.analysis_result_wrapper import AnalysisResultWrapper
+from ra2ce.analysis.analysis_result.analysis_result_wrapper import AnalysisResultWrapper
 from ra2ce.network.graph_files.graph_files_protocol import GraphFileProtocol
 
 
