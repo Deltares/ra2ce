51037917f92301725b6b353dd90dde8abfdb7062
MatthiasHauthDeltares <113418841+MatthiasHauthDeltares@users.noreply.github.com>
2024-12-11 13:01:38 +0100

    Feat/601 create sandbox example input for adaptation run (#645)
    * chore: extend config and reader
    
    * chore: add/correct attributes of config data
    
    * chore: extend reader with options
    
    * Folder structure with input files
    
    * Removing adaptation option input and changing the config file
    
    * Changes to input structure
    
    * Putting input data in the correct location and changes to folder names
    
    * chore: add adaptation property and extend test
    
    * chore: first changes
    
    * chore: add adaptation to enum
    
    * chore: extend classes
    
    * chore: put adaptation in separate enum
    
    * chore: add get_analysis
    
    * chore: remove no_intervention from config
    
    * chore: rename no_intervention to no_adaptation
    
    * Delete ra2ce/analysis/adaptation/adaptation_option_collection.py
    
    * chore: restore no_adaptation_option
    
    * chore: expand logic and add tests
    
    * chore: add losses analysis name to config
    
    * chore: take losses_analysis from config
    
    * chore: fix test
    
    * test: add tests
    
    * chore: fix logic for paths
    
    * chore: small changes
    
    * test: add test data
    
    * chore: small change of folders
    
    * chore: small changes to paths
    
    * chore: small ini change
    
    * chore: typo
    
    * chore: create class and extend factory
    
    * test: fix failing tests
    
    * chore: remove VAT
    
    * chore: add cost calculation
    
    * chore: process review comments
    
    * chore: process review comments
    
    * merge master
    
    * chore: fix issues
    
    * chore: fix test
    
    * updated example, output_graph folder updated
    
    * test: add output_graph files
    
    * test: remove test
    
    * Changes to the notebook
    
    * chore: process review comments
    
    * chore: add base_network in the factory
    
    * chore: add cost calculation
    
    * chore: rename collection attribute
    
    * chore: small changes
    
    * chore: add calculate to collectino
    
    * chore: add root_path
    
    * test: extend conftest
    
    * test: move test input
    
    * chore: add path properties to adaptation_option
    
    * chore: first damages setup
    
    * test: fix/extend tests
    
    * chore: add aggregate_wl to config
    
    * chore: add losses (start)
    
    * chore: big overhaul of the creation of adaptation options
    
    * chore: add docstring
    
    * chore: remove unused imports
    
    * test: fix losses input paths
    
    * chore: fix losses run
    
    * chore: small cleanup
    
    * chore: small changes, add docstring
    
    * chore: fix tests
    
    * chore: small changes
    
    * test: adapt losses configs
    
    * chore: restore graph_file_hazard
    
    * chore: copy static folder as well for avg_speed
    
    * chore: add hazard section with wl_aggregate
    
    * Changes to notebook and input folder structure
    
    * test: correct name of resilience_curve csv
    
    * updated network in test data
    
    * change in naming
    
    * added parameters in the lossess section
    
    * Folder structure revised
    
    * chore: calculate benefit for options
    
    * chore: final changes
    
    * chore: add TODOs
    
    * chore: change readers into dataclasses
    
    * chore: add expected total cost (for now based on unit cost only)
    
    * chore: process rework
    
    * chore/add input files for notebook
    
    * chore/git add ini file
    
    * chore/git add ini file
    
    * chore/add all data for example adaptation
    
    * chore/update notebook and data
    
    * chore/update notebook
    
    * chore/apply review suggestions
    
    * commit notebook again
    
    * chore/remove output test file
    
    * add files to run analysis and losses
    
    * add output_path to Adaptation class
    
    * clear output
    
    * chore/fix notebook
    
    ---------
    
    Co-authored-by: Ardt Klapwijk <ardt.klapwijk@deltares.nl>
    Co-authored-by: Cham8920 <chamidugunaratne8920@gmail.com>
    Co-authored-by: Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>

--- ra2ce/analysis/adaptation/adaptation.py
+++ ra2ce/analysis/adaptation/adaptation.py
@@ -58,6 +58,7 @@ class Adaptation(AnalysisBase, AnalysisDamagesProtocol):
         self.adaptation_collection = AdaptationOptionCollection.from_config(
             analysis_config
         )
+        self.output_path=analysis_input.output_path
 
     def execute(self) -> AnalysisResultWrapper:
         """
