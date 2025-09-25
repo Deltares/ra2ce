cbf93c54ba6bcc9bba964d4b1eea11f242dfcbd1
ArdtK <ardt.klapwijk@deltares.nl>
2025-09-25 14:25:36 +0200

    chore: solve bug from previous PR

--- ra2ce/ra2ce_handler.py
+++ ra2ce/ra2ce_handler.py
@@ -83,12 +83,11 @@ class Ra2ceHandler:
         """
 
         def set_config_paths(_analysis_config: AnalysisConfigWrapper) -> None:
-            if not network:
+            if not isinstance(network, NetworkConfigData):
                 return
             _analysis_config.config_data.root_path = network.root_path
             _analysis_config.config_data.input_path = network.input_path
             _analysis_config.config_data.static_path = network.static_path
-            _analysis_config.config_data.output_path = network.output_path
 
         def get_network_config() -> NetworkConfigWrapper | None:
             if not isinstance(network, NetworkConfigData):
