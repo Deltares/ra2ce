c96d47292c8cab2729704b08db551fa9b7f7087e
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-02 10:23:28 +0200

    chore: remove names properties

--- ra2ce/network/network_config_data/network_config_data.py
+++ ra2ce/network/network_config_data/network_config_data.py
@@ -59,8 +59,6 @@ class NetworkSection:
 class OriginsDestinationsSection:
     origins: Optional[Path] = None
     destinations: Optional[Path] = None
-    origins_name: str = ""
-    destinations_name: str = ""
     origin_count: Optional[str] = None
     origin_out_fraction: int = (
         1  # fraction of things/people going out of the origin to the destination
