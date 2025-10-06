34f2b013eea951aeeae6a8aabe977bca22abd56e
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-06 15:13:23 +0200

    chore: use right dataclass

--- tests/output_validator/file_validators/gpkg_validator.py
+++ tests/output_validator/file_validators/gpkg_validator.py
@@ -1,8 +1,8 @@
+from dataclasses import dataclass
 from pathlib import Path
 
 import fiona
 import geopandas as gpd
-from attr import dataclass
 
 from tests.output_validator.file_validators.file_validator_protocol import (
     FileValidatorProtocol,
@@ -30,14 +30,14 @@ class GpkgValidator(FileValidatorProtocol):
                 f"GPKG file {self.result_file.name} differs in columns: {_ref_schema} != {_res_schema}"
             )
 
-        _first_mismatch = (
-            _gdf_ref.fillna("").ne(_gdf_res.fillna("")).sum(axis=1).idxmax()
-        )
-        if not _first_mismatch:
+        _mismatches = _gdf_ref.fillna("").ne(_gdf_res.fillna(""))
+        _first_mismatch_row = _mismatches.sum(axis=1).idxmax()
+        if not _first_mismatch_row:
             return
 
+        _mismatch_columns = _mismatches.columns[_mismatches.loc[_first_mismatch_row]]
         raise AssertionError(
-            f"GPKG file {self.result_file.name} differs in content:\n"
-            f"Reference: {_gdf_ref.loc[_first_mismatch].to_dict()}\n"
-            f"Result: {_gdf_res.loc[_first_mismatch].to_dict()}"
+            f"GPKG file {self.result_file.name} differs in content on row {_first_mismatch_row}:\n"
+            f"Reference: {_gdf_ref.loc[_first_mismatch_row][_mismatch_columns].to_dict()}\n"
+            f"Result: {_gdf_res.loc[_first_mismatch_row][_mismatch_columns].to_dict()}"
         )
