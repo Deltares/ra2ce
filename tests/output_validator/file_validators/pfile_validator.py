2a3ba5ba7b12a1a1f8a1369c6c2071679f7c4d23
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-06 10:55:16 +0200

    chore: add explicit TODO

--- tests/output_validator/file_validators/pfile_validator.py
+++ tests/output_validator/file_validators/pfile_validator.py
@@ -8,4 +8,5 @@ from tests.output_validator.file_validators.file_validator_protocol import (
 class PfileValidator(FileValidatorProtocol):
     @staticmethod
     def validate(reference_file: Path, result_file: Path) -> None:
-        pass
+        # TODO: implement feather validation. Now only the existence of the file is checked.
+        return
