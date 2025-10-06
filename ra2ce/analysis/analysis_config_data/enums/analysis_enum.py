c46419c68842c47b74615a10ce380e10d2041b38
Ardt Klapwijk <59741981+ArdtK@users.noreply.github.com>
2025-10-06 09:25:56 +0200

    fix: #752 adaptation analysis config not read correctly (#753)
    * fix: parse adaptation analysis correctly
    
    * chore: fix enum warning
    
    * chore: fix reading adaptation bools
    
    * chore: small fix

--- ra2ce/analysis/analysis_config_data/enums/analysis_enum.py
+++ ra2ce/analysis/analysis_config_data/enums/analysis_enum.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+
 from ra2ce.configuration.ra2ce_enum_base import Ra2ceEnumBase
 
 
 class AnalysisEnum(Ra2ceEnumBase):
     ADAPTATION = 2
     INVALID = 99
+
+    @classmethod
+    def get_enum(cls, input_str: str | None) -> AnalysisEnum:
+        return AnalysisEnum(super().get_enum(input_str))
