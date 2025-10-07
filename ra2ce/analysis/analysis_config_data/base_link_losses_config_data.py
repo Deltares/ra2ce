6acd1d9000b38dc58ac58d3a8daf2f96cde23e99
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2025-10-07 11:37:36 +0200

    feat: Create dataclass for `BaseLinkLossesConfigData` (#755)
    * feat: Added new concrete class for base link losses
    
    * chore: Made validation into decorator to reduce code duplication
    
    * chore: reverted using a decorator for validation. Added missing tests
    
    * chore: Renamed test file to match target class name
    
    * test: improved test to validate importing of new dataclasses
    
    * test: Small docstrings update
    
    * test: Removed unnecessary part of the test that would give a false failing status

---
+++ ra2ce/analysis/analysis_config_data/base_link_losses_config_data.py
@@ -0,0 +1,96 @@
+"""
+                GNU GENERAL PUBLIC LICENSE
+                  Version 3, 29 June 2007
+
+Risk Assessment and Adaptation for Critical Infrastructure (RA2CE).
+Copyright (C) 2023 Stichting Deltares
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import math
+from abc import ABC
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Optional
+
+from ra2ce.analysis.analysis_config_data.analysis_config_data_protocol import (
+    AnalysisConfigDataProtocol,
+)
+from ra2ce.analysis.analysis_config_data.enums.event_type_enum import EventTypeEnum
+from ra2ce.analysis.analysis_config_data.enums.risk_calculation_mode_enum import (
+    RiskCalculationModeEnum,
+)
+from ra2ce.analysis.analysis_config_data.enums.traffic_period_enum import (
+    TrafficPeriodEnum,
+)
+from ra2ce.analysis.analysis_config_data.enums.trip_purpose_enum import TripPurposeEnum
+from ra2ce.analysis.analysis_config_data.enums.weighing_enum import WeighingEnum
+from ra2ce.common.validation.validation_report import ValidationReport
+from ra2ce.configuration.legacy_mappers import with_legacy_mappers
+
+
+@with_legacy_mappers
+@dataclass
+class BaseLinkLossesConfigData(AnalysisConfigDataProtocol, ABC):
+    """
+    Reflects all possible settings that a base link losses config might contain.
+    """
+    # Common properties
+    name: str
+    save_gpkg: bool = False
+    save_csv: bool = False
+
+    # Concrete properties
+    event_type: EventTypeEnum = field(default_factory=lambda: EventTypeEnum.NONE)
+    weighing: WeighingEnum = field(default_factory=lambda: WeighingEnum.NONE)
+
+    production_loss_per_capita_per_hour: Optional[float] = math.nan
+    traffic_period: Optional[TrafficPeriodEnum] = field(
+        default_factory=lambda: TrafficPeriodEnum.DAY
+    )
+    trip_purposes: Optional[list[TripPurposeEnum]] = field(
+        default_factory=lambda: [TripPurposeEnum.NONE]
+    )
+    resilience_curves_file: Optional[Path] = None
+    traffic_intensities_file: Optional[Path] = None
+    values_of_time_file: Optional[Path] = None
+
+    # Optional properties
+    risk_calculation_mode: RiskCalculationModeEnum = field(
+        default_factory=lambda: RiskCalculationModeEnum.NONE
+    )
+    # Risk calculation year is required if 'risk_calculation_mode' is set to 'RiskCalculationModeEnum.TRIANGLE_TO_NULL_YEAR'
+    risk_calculation_year: Optional[int] = None
+
+    def validate_integrity(self) -> ValidationReport:
+        _report = ValidationReport()
+        if self.risk_calculation_mode == RiskCalculationModeEnum.TRIANGLE_TO_NULL_YEAR:
+            if self.risk_calculation_year is None or self.risk_calculation_year <= 0:
+                _report.error(
+                    f"For damage analysis '{self.name}': 'risk_calculation_year' should be a positive integer when 'risk_calculation_mode' is set to 'RiskCalculationModeEnum.TRIANGLE_TO_NULL_YEAR'."
+                )
+        return _report
+    
+class MultiLinkLossesConfigData(BaseLinkLossesConfigData):
+    """
+    Configuration data for multi-link losses analysis.
+    """
+    pass
+
+class SingleLinkLossesConfigData(BaseLinkLossesConfigData):
+    """
+    Configuration data for single-link losses analysis.
+    """
+    pass
---
+++ tests/analysis/analysis_config_data/test_base_link_losses_config_data.py
@@ -0,0 +1,131 @@
+from dataclasses import is_dataclass
+
+import pytest
+
+from ra2ce.analysis.analysis_config_data.base_link_losses_config_data import (
+    BaseLinkLossesConfigData,
+    MultiLinkLossesConfigData,
+    SingleLinkLossesConfigData,
+)
+from ra2ce.analysis.analysis_config_data.enums.event_type_enum import EventTypeEnum
+from ra2ce.analysis.analysis_config_data.enums.risk_calculation_mode_enum import (
+    RiskCalculationModeEnum,
+)
+from ra2ce.analysis.analysis_config_data.enums.traffic_period_enum import (
+    TrafficPeriodEnum,
+)
+from ra2ce.analysis.analysis_config_data.enums.trip_purpose_enum import TripPurposeEnum
+from ra2ce.analysis.analysis_config_data.enums.weighing_enum import WeighingEnum
+from ra2ce.common.validation.validation_report import ValidationReport
+
+
+@pytest.mark.parametrize("link_losses_class", [SingleLinkLossesConfigData, MultiLinkLossesConfigData])
+class TestLinkLossesConfigData:
+
+    def test_initialize(self, link_losses_class: type[BaseLinkLossesConfigData]):
+        # 1. Define test data.
+        _data_name = "Test Link Losses Analysis"
+
+        # 2. Run test.
+        link_losses_config = link_losses_class(name=_data_name)
+
+        # 3. Verify expectations.
+        assert isinstance(link_losses_config, link_losses_class)
+        assert is_dataclass(link_losses_config)
+        assert link_losses_config.name == _data_name
+        assert link_losses_config.save_gpkg is False
+        assert link_losses_config.save_csv is False
+        assert link_losses_config.event_type == EventTypeEnum.NONE
+        assert link_losses_config.weighing == WeighingEnum.NONE
+        assert link_losses_config.production_loss_per_capita_per_hour != link_losses_config.production_loss_per_capita_per_hour  # NaN check
+        assert link_losses_config.traffic_period == None or link_losses_config.traffic_period == TrafficPeriodEnum.DAY
+        assert link_losses_config.trip_purposes == None or link_losses_config.trip_purposes == [TripPurposeEnum.NONE]
+        assert link_losses_config.resilience_curves_file is None
+        assert link_losses_config.traffic_intensities_file is None
+        assert link_losses_config.values_of_time_file is None
+        assert link_losses_config.risk_calculation_mode == RiskCalculationModeEnum.NONE
+        assert link_losses_config.risk_calculation_year is None
+
+    @pytest.mark.parametrize(
+        "risk_calculation_year",
+        [
+            pytest.param(None, id="none year"),
+            pytest.param(-5, id="negative year"),
+            pytest.param(0, id="zero year"),
+        ],
+    )
+    def test_given_risk_calculation_triangle_and_invalid_year_when_validate_integrity_then_fails(
+        self, risk_calculation_year: int | None, link_losses_class: type[BaseLinkLossesConfigData]
+    ):
+        # 1. Define test data.
+        _data_name = "Invalid Damages Analysis"
+        _damages_config = link_losses_class(
+            name=_data_name,
+            risk_calculation_mode=RiskCalculationModeEnum.TRIANGLE_TO_NULL_YEAR,
+            risk_calculation_year=risk_calculation_year,
+        )
+
+        _expected_error = f"For damage analysis '{_data_name}': 'risk_calculation_year' should be a positive integer when 'risk_calculation_mode' is set to 'RiskCalculationModeEnum.TRIANGLE_TO_NULL_YEAR'."
+
+        # 2. Run test.
+        _report = _damages_config.validate_integrity()
+
+        # 3. Verify expectations for invalid data.
+        assert isinstance(_report, ValidationReport)
+        assert (
+            not _report.is_valid()
+        ), "Expected invalid report due to negative risk_calculation_year"
+        assert len(_report._errors) == 1
+        assert _report._errors[0] == _expected_error
+
+    def test_given_risk_calculation_triangle_and_valid_year_when_validate_integrity_then_succeeds(
+        self, link_losses_class: type[BaseLinkLossesConfigData]
+    ):
+        # 1. Define test data.
+        _data_name = "Invalid Damages Analysis"
+        _damages_config = link_losses_class(
+            name=_data_name,
+            risk_calculation_mode=RiskCalculationModeEnum.TRIANGLE_TO_NULL_YEAR,
+            risk_calculation_year=1,
+        )
+
+        # 2. Run test.
+        _report = _damages_config.validate_integrity()
+
+        # 3. Verify expectations for invalid data.
+        assert isinstance(_report, ValidationReport)
+        assert (
+            _report.is_valid()
+        ), "Expected valid report due to possitive risk_calculation_year"
+
+    @pytest.mark.parametrize(
+        "risk_calculation_mode",
+        [
+            pytest.param(_mode, id=_mode.name)
+            for _mode in RiskCalculationModeEnum
+            if _mode != RiskCalculationModeEnum.TRIANGLE_TO_NULL_YEAR
+        ],
+    )
+    @pytest.mark.parametrize("risk_calculation_year", [(None), (-5), (0), (2)])
+    def test_given_any_non_triangle_risk_calculation_when_validate_integrity_then_succeeds(
+        self,
+        risk_calculation_mode: RiskCalculationModeEnum,
+        risk_calculation_year: int | None,
+        link_losses_class: type[BaseLinkLossesConfigData]
+    ):
+        # 1. Define test data.
+        _data_name = "Valid Damages Analysis"
+        _damages_config = link_losses_class(
+            name=_data_name,
+            risk_calculation_mode=risk_calculation_mode,
+            risk_calculation_year=risk_calculation_year,
+        )
+
+        # 2. Run test.
+        _report = _damages_config.validate_integrity()
+
+        # 3. Verify expectations for invalid data.
+        assert isinstance(_report, ValidationReport)
+        assert (
+            _report.is_valid()
+        ), "Expected valid report due to non-triangle risk calculation mode"
