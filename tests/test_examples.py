19c64d9412e0788775793644efc0d2ef00dd2976
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-08 13:21:22 +0200

    chore: validate examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 import pytest
@@ -5,6 +6,7 @@ from pytest_notebook.execution import execute_notebook
 from pytest_notebook.notebook import load_notebook
 
 from tests import test_examples
+from tests.output_validator.output_validator import OutputValidator
 
 _supported_examples = lambda x: "DIY" not in x.stem
 _jupyter_examples = [
@@ -34,3 +36,23 @@ class TestExamples:
             # If execution errors were found then raise the exception so the
             # output looks "beautiful", instead of using `pytest.raises(...)`
             raise _execution_result.exec_error
+
+        def _get_path(input_str: str) -> Path:
+            # Strip of spaces, quotes and slashes, then split on commas and join again.
+            _parts = [s.strip(' "\\') for s in input_str.split(",")]
+            return Path().joinpath(*_parts)
+
+        def _find_root_dirs() -> list[Path]:
+            # Look for lines that set the root_dir variable.
+            _jupyter_content = jupyter_example.read_text()
+            _matches = re.findall(r"root_dir\s?=\s?Path\((.*?)\)", _jupyter_content)
+            if _matches:
+                return [_get_path(_match) for _match in _matches]
+            return []
+
+        # Validate the output(s) of the notebook against the reference output(s).
+        for _root_dir in _find_root_dirs() or []:
+            _example_dir = test_examples.joinpath(_root_dir)
+            if not _example_dir or not _example_dir.joinpath("reference").is_dir():
+                continue
+            OutputValidator(_example_dir).validate_output()
