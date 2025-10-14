bcac73f81dd3171e21a683741bc55c9af3d040f0
Carles S. Soriano Pérez <sorianoperez.carles@gmail.com>
2025-10-14 11:59:59 +0000

    chore: Explicit imports at the subproject 'analysis_config_data'

--- ra2ce/analysis/analysis_config_data/__init__.py
+++ ra2ce/analysis/analysis_config_data/__init__.py
@@ -1,1 +1,27 @@
 
+from .adaptation_config_data import AdaptationConfigData
+from .base_link_losses_config_data import (
+    MultiLinkLossesConfigData,
+    SingleLinkLossesConfigData,
+)
+from .base_origin_destination_config_data import (
+    MultiLinkOriginClosestDestinationConfigData,
+    MultiLinkOriginDestinationConfigData,
+    OptimalRouteOriginClosestDestinationConfigData,
+    OptimalRouteOriginDestinationConfigData,
+)
+from .damages_config_data import DamagesConfigData
+from .equity_config_data import EquityConfigData
+from .losses_analysis_config_data_protocol import LossesAnalysisConfigDataProtocol
+from .multi_link_redundancy_config_data import MultiLinkRedundancyConfigData
+
+LossesConfigData: list[type[LossesAnalysisConfigDataProtocol]] = [
+    SingleLinkLossesConfigData,
+    MultiLinkLossesConfigData,
+    MultiLinkRedundancyConfigData,
+    MultiLinkOriginDestinationConfigData,
+    MultiLinkOriginClosestDestinationConfigData,
+    OptimalRouteOriginDestinationConfigData,
+    OptimalRouteOriginClosestDestinationConfigData,
+    EquityConfigData
+]
