d756c5cc60fad1c8687b6e83ffde7f78b614a9cc
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-15 11:19:59 +0200

    chore: experiment with p-file validation

--- tests/output_validator/file_validators/pfile_validator.py
+++ tests/output_validator/file_validators/pfile_validator.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from pathlib import Path
 
 import networkx as nx
-import pandas as pd
+import numpy as np
 
 from tests.output_validator.file_validators.file_validator_protocol import (
     FileValidatorProtocol,
@@ -16,16 +16,16 @@ class PfileValidator(FileValidatorProtocol):
     result_file: Path
 
     def __post_init__(self) -> None:
-        def _get_normalized_content(file_path: Path) -> pd.DataFrame:
+        def _get_normalized_content(file_path: Path) -> np.ndarray:
             with open(file_path, "rb") as _file_path:
                 _graph = pickle.load(_file_path)
             assert isinstance(_graph, nx.Graph)
-            return _graph
+            return nx.to_numpy_array(_graph, weight="weight")
 
-        _graph_ref = _get_normalized_content(self.reference_file)
-        _graph_res = _get_normalized_content(self.result_file)
+        _nodes_ref = _get_normalized_content(self.reference_file)
+        _nodes_res = _get_normalized_content(self.result_file)
 
-        if nx.is_isomorphic(_graph_ref, _graph_res):
+        if np.allclose(_nodes_ref, _nodes_res, atol=1e-3):
             return
 
         raise AssertionError(f"P file {self.result_file.name} deviates in content.")
