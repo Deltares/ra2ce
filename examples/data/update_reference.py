0ce4f32ee0382e5b988305c817f3dc25c9ace4d3
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-15 09:28:17 +0200

    chore: add script to update example reference data from output

---
+++ examples/data/update_reference.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from shutil import copy
+
+from tests.output_validator.output_validator import OutputValidator
+
+
+class UpdateReference:
+    example_data_path: Path
+
+    def __init__(self):
+        self.example_data_path = Path(__file__).parent
+
+    def update(self) -> None:
+        for _example_data in self.example_data_path.iterdir():
+            for _reference_file in OutputValidator._get_relative_paths(
+                _example_data.joinpath("reference")
+            ):
+                _from = self.example_data_path.joinpath(_example_data, _reference_file)
+                _to = self.example_data_path.joinpath(
+                    _example_data, "reference", _reference_file
+                )
+                _to.parent.mkdir(parents=True, exist_ok=True)
+                if _from.is_file():
+                    copy(_from, _to)
+
+
+UpdateReference().update()
