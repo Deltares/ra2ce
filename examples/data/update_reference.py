5426c3d2733496de8cb84d1d17fb98f9a26b2ee5
ArdtK <ardt.klapwijk@deltares.nl>
2025-10-16 10:58:42 +0200

    chore: remove class

--- examples/data/update_reference.py
+++ examples/data/update_reference.py
@@ -4,28 +4,21 @@ from shutil import copy
 from tests.output_validator.output_validator import OutputValidator
 
 
-class UpdateReference:
-    example_data_path: Path
-
-    def __init__(self):
-        self.example_data_path = Path(__file__).parent
-
-    def update(self) -> None:
-        for _example_data in self.example_data_path.iterdir():
-            _example_reference = _example_data.joinpath("reference")
-            if not _example_reference.is_dir():
-                continue
-            for _reference_file in OutputValidator._get_relative_paths(
-                _example_reference
-            ):
-                _from = self.example_data_path.joinpath(_example_data, _reference_file)
-                _to = self.example_data_path.joinpath(
-                    _example_data, "reference", _reference_file
-                )
-                _to.parent.mkdir(parents=True, exist_ok=True)
-                if _from.is_file():
-                    copy(_from, _to)
-
-
 def run() -> None:
-    UpdateReference().update()
+    _example_data_path = Path(__file__).parent
+    for _example_data in _example_data_path.iterdir():
+        _example_reference = _example_data.joinpath("reference")
+        if not _example_reference.is_dir():
+            continue
+        for _reference_file in OutputValidator._get_relative_paths(_example_reference):
+            _from = _example_data_path.joinpath(_example_data, _reference_file)
+            _to = _example_data_path.joinpath(
+                _example_data, "reference", _reference_file
+            )
+            _to.parent.mkdir(parents=True, exist_ok=True)
+            if _from.is_file():
+                copy(_from, _to)
+
+
+if __name__ == "__main__":
+    run()
