8ef1c32629c7c755e51d8e2ac3363c795cd85f88
Carles S. Soriano Pérez <sorianoperez.carles@gmail.com>
2026-02-05 13:02:23 +0100

    test: Adapted test to support multiple python version (testing)

--- tests/analysis/losses/test_losses_input_data_reader_base.py
+++ tests/analysis/losses/test_losses_input_data_reader_base.py
@@ -7,12 +7,21 @@ from ra2ce.analysis.losses.losses_input_data_reader_base import (
 
 class TestLossesInputDataReaderBase:
     def test_initialize_base_raises(self):
-        # 1. Run test
+        # 1. Define test data
+        # Disclaimer: Done like this as in Python 3.11 the exception error differs slightly.
+        _expected_class_str = f"Can't instantiate abstract class {LossesInputDataReaderBase.__name__}"
+        _expected_methods_str = "abstract method"
+        _expected_methods_list = [
+            "_parse_df"
+        ]
+
+        # 2. Run test
         with pytest.raises(TypeError) as exc_info:
             LossesInputDataReaderBase()
 
-        # 2. Verify expectations
-        assert (
-            str(exc_info.value)
-            == f"Can't instantiate abstract class {LossesInputDataReaderBase.__name__} with abstract method _parse_df"
-        )
+        # 3. Verify expectations
+        _exception_mssg = str(exc_info.value)
+        assert _expected_class_str in _exception_mssg
+        assert _expected_methods_str in _exception_mssg
+        for method in _expected_methods_list:
+            assert method in _exception_mssg
--- tests/analysis/losses/traffic_analysis/test_traffic_analysis_base.py
+++ tests/analysis/losses/traffic_analysis/test_traffic_analysis_base.py
@@ -10,10 +10,22 @@ class TestTrafficAnalysisBase:
         """
         This test verifies that `TrafficAnalysisBase` is (and will be) an abstract class (`abc.ABC`).
         """
+        # 1. Define test data
+        # Disclaimer: Done like this as in Python 3.11 the exception error differs slightly.
+        _expected_class_str = f"Can't instantiate abstract class {TrafficAnalysisBase.__name__}"
+        _expected_methods_str = "abstract methods"
+        _expected_methods_list = [
+            "_get_accumulated_traffic_from_node",
+            "_get_route_traffic"
+        ]
+
+        # 2. Run test
         with pytest.raises(TypeError) as exc_err:
             TrafficAnalysisBase()
 
-        assert (
-            str(exc_err.value)
-            == "Can't instantiate abstract class TrafficAnalysisBase with abstract methods _get_accumulated_traffic_from_node, _get_route_traffic"
-        )
+        # 3. Verify expectations
+        _exception_mssg = str(exc_err.value)
+        assert _expected_class_str in _exception_mssg
+        assert _expected_methods_str in _exception_mssg
+        for method in _expected_methods_list:
+            assert method in _exception_mssg
